81c97f6206fe981403b36cef3e1a96aa15c85770
Ben Nowotny <bennowotny65535@gmail.com>
2020-11-04 20:01:22 -0600

    Updated Angle Calculation logic to better accommodate the curvature of the Earth.

--- src/wr_logic_ai/src/angle_calculations.py
+++ src/wr_logic_ai/src/angle_calculations.py
@@ -1,7 +1,10 @@
 import math
+import numpy as np
+
 
 class AngleCalculations:
     EARTH_RADIUS = 6378100
+    SPHERICAL_EAST = [1,0,math.pi/2] # rho, theta, phi
 
     def __init__(self, clatitude, clongitude, glatitude, glongitude):
         self.cur_lat = clatitude
@@ -13,34 +16,30 @@ class AngleCalculations:
         self.down = False
         self.left = False
 
-
-    def latitude_to_distance(self, lat1, lat2):
-        if(self.cur_lat < self.tar_lat):
-            self.up = True
-        else:
-            self.down = True
-
-        deltaLatitude = math.radians(math.fabs(lat2 - lat1))
-        verticalDistance = math.pow(math.sin(deltaLatitude / 2.0), 2)
-        angularDistance = 2 * math.atan2(math.sqrt(verticalDistance), math.sqrt(1.0 - verticalDistance))
-        return angularDistance * self.EARTH_RADIUS
-
-    def longitude_to_distance(self, lon1, lon2):
-        if(self.cur_long < self.tar_long):
-            self.right = True
-        else:
-            self.left = True;
-
-        deltaLongitude = math.radians(math.fabs(lon2 - lon1))
-        lateralDistance = math.cos(math.radians(self.cur_lat)) * math.cos(math.radians(self.tar_lat)) * math.pow(math.sin(deltaLongitude / 2.0), 2)
-        angularDistance = 2 * math.atan2(math.sqrt(lateralDistance), math.sqrt(1.0 - lateralDistance))
-        return angularDistance * self.EARTH_RADIUS
-
-    def get_distance(self):
-        return math.sqrt(math.pow(self.latitude_to_distance(self.cur_lat, self.tar_lat), 2) + math.pow(self.longitude_to_distance(self.cur_long, self.tar_long), 2))
-
+    def spherical_to_cartesian(coord: list) -> np.ndarray:
+        rho = coord[0]
+        theta = coord[1]
+        phi = coord[2]
+        return rho*np.array([math.cos(theta)*math.sin(phi), math.sin(theta)*math.sin(phi), math.cos(phi)])
+    
     def get_angle(self):
-        angle = math.atan(self.latitude_to_distance(self.cur_lat, self.tar_lat) / self.longitude_to_distance(self.cur_long, self.tar_long))
+        curr_coords = [1].append(list(map(lambda i: math.radians(i), [90 - self.cur_lat, self.cur_long])))
+        targ_coords = [1].append(list(map(lambda i: math.radians(i), [90 - self.tar_lat, self.tar_long])))
+
+        a = AngleCalculations.spherical_to_cartesian(SPHERICAL_EAST)
+        b = AngleCalculations.spherical_to_cartesian(curr_coords)
+        c = AngleCalculations.spherical_to_cartesian(targ_coords)
+
+        n1 = np.cross(a, b)
+        n2 = np.cross(b, c)
+
+        angle = math.acos(np.dot(n1, n2)/(np.sqrt(np.dot(n1, n1) * np.sqrt(np.dot(n2, n2)))))
+        
+        self.up = self.cur_lat < self.tar_lat
+        self.down = not self.up
+        self.right = self.cur_long < self.tar_long
+        self.left = not self.right
+        
         angle = math.degrees(angle)
         if (self.left and self.up):
             angle = 180.0 - angle
