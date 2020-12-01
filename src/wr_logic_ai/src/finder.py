bc96908f4c7554aedff0eff4aa55e305fb3e8bab
Ben Nowotny <bennowotny65535@gmail.com>
2020-11-30 18:45:50 -0600

    Added comments and most type hints to finder.py

--- src/wr_logic_ai/src/finder.py
+++ src/wr_logic_ai/src/finder.py
@@ -1,24 +1,29 @@
+from typing import List
 import histogram
 import math
 
 import pickle
 
-# returns distance (in degrees) from sector parameter to target
-# returns 0 if sector parameter contains target
-
-
-def get_target_distance(sector_i, sector_f, target, sector_angle):
-    # GETTING NONE FROM SECTOR VARIABLES
+# Returns angular distance (in degrees) from sector parameter to target
+# Returns 0 if sector parameter contains target
+# TODO: Is there a cleaner way to write the logic?
+def get_target_distance(sector_i: int, sector_f: int, target: int, sector_angle: float) -> float:
+    # Compute if the target is under the starting sector
     if target < sector_i:
+        # Return angular distance between target and starting sector
         return (sector_i - target) * sector_angle
+    # Compute if the target is above the last sector
     if target > sector_f:
+        # Return the angular distance between the target and last sector
         return (target - sector_f) * sector_angle
+    # Otherwise, the target is within the sector bounds
+    # Angular distance is then 0
     return 0
 
-# number of consecutive sectors where POD is below threshold
 
-
-def is_wide_valley(sector_i, sector_f, max_valley):
+# Returns whether or not a valley is as wide or wider than the recommended max valley size
+def is_wide_valley(sector_i: int, sector_f: int, max_valley: int) -> bool:
+    #TODO: Cleaner way to write this?
     return 1 + sector_f - sector_i > max_valley
 
 # represent a valley as an ordered pair as in (start sector, end sector)
@@ -27,62 +32,89 @@ def is_wide_valley(sector_i, sector_f, max_valley):
 # valleys of any length should be valid since they can only come from smoothing function of adjacent areas
 # and correct thresholding
 
-
+# Get the 'best' valley from the LIDAR data.  The valley is formatted as [Start Sector, End Sector]
 def get_valley(
-        target,
-        sector_count,
-        sector_angle,
-        threshold,
-        vision_angle,
+        target: int,
+        sector_count: int,
+        sector_angle: float,
+        threshold: float,
+        vision_angle: int,
         data,
-        smoothing=3):
+        smoothing: float = 3) -> List[int]:
+
+    # Get the smoothed histogram of the vision data
     hist = histogram.Histogram(
         sector_count,
         threshold,
         vision_angle,
         data,
-        smoothing)  # TODO histogram and parameters
+        smoothing)
+
+    # Write the sectors data to an output file for logging
     output_file = open('sectors.data', 'wb')
     pickle.dump(hist.sectors, output_file)
     output_file.close()
 
+    # Initialize the target valley to the worst conditions so that it is immediately overriden
     valley_start = None
     best_valley = [0, len(hist.sectors) - 1]
-    # the furthest a valley could every be from is 360 degrees since this is a
-    # circle
+    # The furthest a valley could every be from is 360 degrees since this is a circle
     best_distance = 361
+
+    # For each sector in the histogram...
     for i in range(len(hist.sectors)):
+
+        # If the start of the valley hasn't been set yet...
         if valley_start is None:
+            # ...and the sector is below the threshold...
             if hist.sectors[i] < threshold:
+                # Start the valley at the current sector
                 valley_start = i
+
+        # If the start of the valley has been set and the current sector is above the threshold...
         elif hist.sectors[i] > threshold:
+            # Get the effective distance between the target angle and the sector
             dist = get_target_distance(valley_start, i, target, sector_angle)
+
+            # If current valley is closer to the target than the current best valley...
             if dist < best_distance:
+                # Replace the best distance and best valley
                 best_distance = dist
                 best_valley = [valley_start, i]
 
+            # Since we are above the threshold limit, end the current valley
             valley_start = None
 
+    # If a valley was started near the end of the vision range...
     if valley_start is not None:
+        # Check the distance between the target angle and a valley starting at the recorded position and ending at the end of the view frame
         dist = get_target_distance(
             valley_start, sector_count, target, sector_angle)
+        
+        # If that distance was better than the current best sector distance...
         if dist < best_distance:
+            # Set the best valley to the measured valley
             best_valley = [valley_start, i]
 
     # TODO: REVERSE DIRECTION IN CASE OF EMPTY VALLEY if best_valley is [None,
     # None]:
+    # TODO: Implement play_navigation (drive in reverse w.r.t. log file)
+
+    # Return the sectors defining the best valley
     return best_valley
 
 
 # Gets the best angle to navigate to
 def get_navigation_angle(
-        target,
-        sector_count,
-        sector_angle,
-        threshold,
-        vision_angle,
+        target: int,
+        sector_count: int,
+        sector_angle: int,
+        threshold: float,
+        vision_angle: int,
         data,
-        smoothing_constant = 3):
+        smoothing_constant:float = 3) -> float:
+
+    # Get the best valley in the LIDAR data given our target angle
     best_valley = get_valley(
         target,
         sector_count,
@@ -91,25 +123,47 @@ def get_navigation_angle(
         vision_angle,
         data,
         smoothing = smoothing_constant)
+
+    # Define the difference between 'wide' and 'narrow' valleys
+    # For wide valleys, we want to drive on the edge of the valley
+    #   i.e. Don't drive to the middle of a field just to avoid a pole
+    # For narrow valleys, we want to drive in the middle of a valley
+    #   i.e. We don't want to cut it too close on a doorway
+
     # research suggest that the biggest valley should not be bigger than 90
     # degrees
     max_valley = int(90 / sector_angle)
-    # if target and best valley are the same
+
+    # If the target is already in the best valley...
     if get_target_distance(
             best_valley[0],
             best_valley[1],
             target,
             sector_angle) == 0:
+
+        # Report the current target angle; no adjustment needed
         return target * sector_angle
+
+    # If the valley is wide...
     elif is_wide_valley(best_valley[0], best_valley[1], max_valley):
-        # valley is big, we just set target at the closest safe angle
+        # Follow the protocol as defined above for wide valleys
+        
+        # Get the nearest edge of the sector
         nearest_sector = best_valley[1] if target > best_valley[1] else best_valley[0]
+        
+        # Construct a second border edge to make the valley of size max_valley
         border_sector = best_valley[1] - \
             max_valley if target > best_valley[1] else best_valley[0] + max_valley
+        # Ensure that this new border edge is within the bounds of allowable sectors
         border_sector = 0 if border_sector < 0 else border_sector
         border_sector = sector_count if border_sector > sector_count else border_sector
+
+        # Aim for the center of this new max_valley valley (this helps avoid accidentally clipping an edge of the robot)
         return ((nearest_sector + border_sector) / 2.0) * sector_angle
 
+    # If the valley is narrow...
     else:
-        # valley is small, centers the robot in the middle of the valley
+        # Follow the probotcol as defined above for narrow valleys
+        
+        #Aim for the center of the valley
         return ((best_valley[0] + best_valley[1]) / 2.0) * sector_angle
