2ca7b10d6cebb27795ae33b80fba1fe2adac2593
Ben Nowotny <bennowotny65535@gmail.com>
2021-03-10 19:52:13 -0600

    Added ArmControlSystem source file with default logic for a simple action server.

--- src/wr_control_arm/src/ArmControlSystem.cpp
+++ src/wr_control_arm/src/ArmControlSystem.cpp
@@ -1,1 +1,19 @@
-#include "ros.h"
+#include "ros/ros.h"
+#include <control_msgs/FollowJointTrajectoryAction.h>
+#include <actionlib/server/simple_action_server.h>
+
+typedef actionlib::SimpleActionServer<control_msgs::FollowJointTrajectoryAction> Server;
+
+void executeAction(const control_msgs::FollowJointTrajectoryActionGoalConstPtr& goal, Server* s){
+    s->setSucceeded();
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "ArmControlSystem");
+  ros::NodeHandle n;
+  Server server(n, "/arm_controller/follow_joint_trajectory", boost::bind(&executeAction, _1, &server), false);
+  server.start();
+  ros::spin();
+  return 0;
+}
