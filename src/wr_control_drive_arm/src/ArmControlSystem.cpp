d1ae96faf1144818c0f9b02d099648e68c409615
Ben Nowotny <bennowotny65535@gmail.com>
2021-03-22 19:25:27 -0500

    Control loop added, missing ROS logic (spins and such) and publishing TF/Joint State data.

--- src/wr_control_drive_arm/src/ArmControlSystem.cpp
+++ src/wr_control_drive_arm/src/ArmControlSystem.cpp
@@ -7,15 +7,21 @@ ArmMotor motors[6];
 typedef actionlib::SimpleActionServer<control_msgs::FollowJointTrajectoryAction> Server;
 
 void execute(const control_msgs::FollowJointTrajectoryGoalConstPtr& goal, Server* as) {
-    // Do lots of awesome groundbreaking robot stuff here
-    //TODO: Execute arm actions in goal->trajectory.points[].position
     for(int i = 0; i < goal->trajectory.points.size(); i++){
       trajectory_msgs::JointTrajectoryPoint currTargetPosition = goal->trajectory.points[i];
       for(int j = 0; j < currTargetPosition.positions.size(); j++){
         motors[j].runToTarget(currTargetPosition.positions[j], 0.1);
       }
+
       bool hasPositionFinished = false;
-      //TODO: Implement checking each motor for completed position actions.
+      while(!hasPositionFinished){
+        bool temp = true;
+        for(int j = 0; j < currTargetPosition.positions.size(); j++){
+          motors[j].runToTarget(currTargetPosition.positions[j], 0.1);
+          temp &= motors[j].getMotorState() == MotorState::STOP;
+        }
+        hasPositionFinished = temp;
+      }
     }
     as->setSucceeded();
 }
@@ -23,6 +29,7 @@ void execute(const control_msgs::FollowJointTrajectoryGoalConstPtr& goal, Server
 
 int main(int argc, char** argv)
 {
+
   ros::init(argc, argv, "ArmControlSystem");
   ros::NodeHandle n;
   Server server(n, "/arm_controller/follow_joint_trajectory", boost::bind(&execute, _1, &server), false);
