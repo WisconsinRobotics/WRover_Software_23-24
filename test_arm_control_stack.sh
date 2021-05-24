0502500b8f2b75db911a76b7a423f2541a632005
Ben Nowotny <bennowotny65535@gmail.com>
2021-05-24 18:44:10 -0500

    Adding rqt_plot to the arm launch script to see joint state data and controller responses.

--- test_arm_control_stack.sh
+++ test_arm_control_stack.sh
@@ -14,4 +14,5 @@ export WROVER_LOCAL=true
 gnome-terminal -- roslaunch wr_entry_point test_arm.launch
 gnome-terminal -- roslaunch wr_control_drive_arm std.launch
 gnome-terminal -- roslaunch wroboarm_21 demo_test.launch
+gnome-terminal -- rqt_plot /control/arm/00/setpoint /control/arm/00/feedback
 cd -
