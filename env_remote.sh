dd9bc10d5a84c2cd6fbaae72214b57f8e0b99abb
phantamanta44 <evandalong@gmail.com>
2021-05-26 20:37:12 -0500

    Use env_remote.sh to set environment on remote

---
+++ env_remote.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ] ; then
+  /bin/echo "Usage: env.sh COMMANDS"
+  exit 1
+fi
+
+CATKIN_SHELL=sh
+_CATKIN_SETUP_DIR=$(cd "`dirname "$0"`" > /dev/null && pwd)
+. "$_CATKIN_SETUP_DIR/setup.sh"
+
+export ROS_MASTER_URI='http://wrover-nano.local:11311'
+export ROS_HOSTNAME='wrover-nano.local'
+
+exec "$@"
