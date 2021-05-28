90ad742baa89cbdf38e4f65e8df0045ae55b5de3
Ben Nowotny <bennowotny65535@gmail.com>
2021-05-27 20:30:19 -0500

    Modified stepping logic and how the test simulation works.

--- src/wr_logic_teleop_arm/src/ArmTeleopLogic.cpp
+++ src/wr_logic_teleop_arm/src/ArmTeleopLogic.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <iostream>
 #include <moveit/move_group_interface/move_group_interface.h>
 
 int main(int argc, char** argv){
@@ -9,9 +10,24 @@ int main(int argc, char** argv){
 
     moveit::planning_interface::MoveGroupInterface move("arm");
     // move.setPlannerId("RRTStar");
-    move.setPlanningTime(0.5);
-    move.setPositionTarget(0.35, 0.35, 0.35);
-    move.move();
+    move.setPlanningTime(0.15);
+    double x = -0.5;
+    int i = 0;
+    while(x<0.5){
+        geometry_msgs::PoseStamped p;
+        p.pose.position.x = 0.25;
+        p.pose.position.y = x;
+        p.pose.position.z = 0.25;
+        p.pose.orientation.x = 0;
+        p.pose.orientation.y = sin(-M_PI/4);
+        p.pose.orientation.z = 0;
+        p.pose.orientation.w = cos(-M_PI/4);
+        p.header.frame_id = "odom_combined";
+        move.setPoseTarget(p);
+        move.move();
+        x+=0.05;
+        i++;
+    }
 
     return 0;
 }
