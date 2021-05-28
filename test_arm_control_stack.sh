9523473b69c2763bc724cac896f27f1b281a24e4
Ben Nowotny <bennowotny65535@gmail.com>
2021-05-27 20:31:40 -0500

    Disabled `rqt_plot` tracking by comment, lengthened the timing to start the ArmTeleopLogic `rosrun`.

--- test_arm_control_stack.sh
+++ test_arm_control_stack.sh
@@ -14,7 +14,7 @@ export WROVER_LOCAL=true
 gnome-terminal -- roslaunch wr_entry_point test_arm.launch
 gnome-terminal -- roslaunch wr_control_drive_arm std.launch
 gnome-terminal -- roslaunch wroboarm_21 demo_test.launch
-gnome-terminal -- rqt_plot /control/arm/00/setpoint /control/arm/00/feedback
-sleep 5
+#gnome-terminal -- rqt_plot /control/arm/00/setpoint /control/arm/00/feedback
+sleep 10
 gnome-terminal -- /bin/sh -c "rosrun wr_logic_teleop_arm ArmTeleopLogic; while true; do sleep 1; done"
 cd -
