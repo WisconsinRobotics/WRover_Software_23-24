0ff1109990ce038db3e81165a0caea6db6251e6b
Ben Nowotny <bennowotny65535@gmail.com>
2021-07-15 21:17:52 -0500

    Added controller functionality for the Arm (yay!).  Tested with XBox-like controller.

--- src/wr_logic_teleop_arm/src/ArmTeleopLogic.cpp
+++ src/wr_logic_teleop_arm/src/ArmTeleopLogic.cpp
@@ -1,23 +1,61 @@
+#include "ros/rate.h"
+#include "ros/subscriber.h"
+#include "std_msgs/Bool.h"
+#include "std_msgs/Float32.h"
+#include "boost/function.hpp"
+#include <cstdint>
 #include <ros/ros.h>
-#include <iostream>
 #include <moveit/move_group_interface/move_group_interface.h>
 
-int main(int argc, char** argv){
+using Std_Bool = const std_msgs::BoolConstPtr&;
+using Std_Float32 = const std_msgs::Float32ConstPtr&;
+
+auto main(int argc, char** argv) -> int{
     ros::init(argc, argv, "ArmTeleopLogic");
     ros::AsyncSpinner spin(1);
     spin.start();
-    ros::NodeHandle np("~");
+    ros::NodeHandle np("~"); 
+
+    constexpr float PLANNING_TIME = 0.05;
+    constexpr float CLOCK_RATE = 50;
+    constexpr uint32_t MESSAGE_QUEUE_LENGTH = 1000; 
+    constexpr float TRIGGER_PRESSED = 0.5;
+
+    constexpr float STEP_X = 0.001;
+    constexpr float STEP_Y = 0.001;
+    constexpr float STEP_Z = 0.001;
+
+    constexpr float HOME_X = 0.25;
+    constexpr float HOME_Y = 0;
+    constexpr float HOME_Z = 0.25;
+
+    float x_pos = HOME_X;
+    float y_pos = HOME_Y;
+    float z_pos = HOME_Z;
 
     moveit::planning_interface::MoveGroupInterface move("arm");
     // move.setPlannerId("RRTStar");
-    move.setPlanningTime(0.15);
-    double x = -0.5;
-    int i = 0;
-    while(x<0.5){
-        geometry_msgs::PoseStamped p;
-        p.pose.position.x = 0.25;
-        p.pose.position.y = x;
-        p.pose.position.z = 0.25;
+    move.setPlanningTime(PLANNING_TIME);
+    ros::Rate loop {CLOCK_RATE};
+
+    ros::Subscriber yAxis = np.subscribe("/xbox_test/axis/pov_y", 
+        MESSAGE_QUEUE_LENGTH, 
+        static_cast<boost::function<void(Std_Float32)>>([&](Std_Float32 msg) -> void {y_pos += static_cast<bool>(msg->data) ? msg->data > 0 ? STEP_Y : -STEP_Y : 0;}));
+    ros::Subscriber xAxis = np.subscribe("/xbox_test/axis/pov_x", 
+        MESSAGE_QUEUE_LENGTH, 
+        static_cast<boost::function<void(Std_Float32)>>([&](Std_Float32 msg) -> void {x_pos += static_cast<bool>(msg->data) ? msg->data > 0 ? STEP_X : -STEP_X : 0;}));
+    ros::Subscriber zUp = np.subscribe("/xbox_test/button/shoulder_l",
+        MESSAGE_QUEUE_LENGTH,
+        static_cast<boost::function<void(Std_Bool)>>([&](Std_Bool msg) -> void {z_pos += msg->data ? STEP_Z : 0;}));
+    ros::Subscriber zDown = np.subscribe("/xbox_test/axis/trigger_l",
+        MESSAGE_QUEUE_LENGTH,
+        static_cast<boost::function<void(Std_Float32)>>([&](Std_Float32 msg) -> void {z_pos += msg->data > TRIGGER_PRESSED ? -STEP_Z : 0;}));
+
+    while(ros::ok()){
+        geometry_msgs::PoseStamped p {};
+        p.pose.position.x = x_pos;
+        p.pose.position.y = y_pos;
+        p.pose.position.z = z_pos;
         p.pose.orientation.x = 0;
         p.pose.orientation.y = sin(-M_PI/4);
         p.pose.orientation.z = 0;
@@ -25,8 +63,7 @@ int main(int argc, char** argv){
         p.header.frame_id = "odom_combined";
         move.setPoseTarget(p);
         move.move();
-        x+=0.05;
-        i++;
+        loop.sleep();
     }
 
     return 0;
