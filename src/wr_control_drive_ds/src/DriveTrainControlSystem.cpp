48a2f2281db54367a49d2ef45def29a30136780c
Ben Nowotny <bennowotny65535@gmail.com>
2022-03-25 19:47:28 -0500

    Passing lint for the control layer

--- src/wr_control_drive_ds/src/DriveTrainControlSystem.cpp
+++ src/wr_control_drive_ds/src/DriveTrainControlSystem.cpp
@@ -4,10 +4,14 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <array>
 
 using namespace std;
 // Store the input abstract power values
-float inp_vals[2];
+std::array<float, 2> inp_vals{0.0, 0.0};
+
+constexpr std::uint32_t MESSAGE_CACHE_SIZE = 10;
+constexpr float CONTROL_LOOP_RATE = 50.0;
 
 // Reads messages from the /control/drive_train_cmd topic as abstract power values (Float32 on [-1, 1])
 void setDSPower_callback(const wr_drive_msgs::DriveTrainCmd::ConstPtr& msg){
@@ -26,13 +30,13 @@ int main(int argc, char** argv){
 	ros::NodeHandle n;
 
 	// Create a subscriber to the input topic for power values
-	ros::Subscriber inTopic = n.subscribe("/control/drive_system/cmd", 1000, setDSPower_callback);
+	ros::Subscriber inTopic = n.subscribe("/control/drive_system/cmd", MESSAGE_CACHE_SIZE, setDSPower_callback);
 
 	// Create a publisher to the output topic for WRoboClaw duty cycle numbers for command 34
-	ros::Publisher outTopic = n.advertise<wroboclaw::Int16Pair>("/hsi/roboclaw/drive/cmd", 1000);
+	ros::Publisher outTopic = n.advertise<wroboclaw::Int16Pair>("/hsi/roboclaw/drive/cmd", MESSAGE_CACHE_SIZE);
 
 	// Create the processing rate (50 Hz)
-	ros::Rate loop(50);
+	ros::Rate loop(CONTROL_LOOP_RATE);
 
 	// Create the (resuable) output message
 	wroboclaw::Int16Pair output;
