6de943b7e723ba0eb6562c1e72a109886cca45dd
Arthur Wang <arwang5@wisc.edu>
2022-04-11 17:44:52 -0700

    Incomplete de-linting on ArmControlSystem

--- src/wr_control_drive_arm/src/ArmControlSystem.cpp
+++ src/wr_control_drive_arm/src/ArmControlSystem.cpp
@@ -27,20 +27,30 @@ using XmlRpc::XmlRpcValue;
 /**
  * @brief Refresh rate of ros::Rate
  */
-const float CLOCK_RATE = 2;
+constexpr float CLOCK_RATE = 2;
+
+/**
+ * @brief Nessage cache size of publisher
+ */
+constexpr std::uint32_t MESSAGE_CACHE_SIZE = 1000; 
+
+/**
+ * @brief Period between timer callback
+ */
+constexpr float TIMER_CALLBACK_DURATION = 1.0 / 50.0;
 
 /**
  * @brief Defines space for all ArmMotor references
  */
-const int numMotors = 6;
-std::array<std::unique_ptr<ArmMotor>, numMotors> motors;
+constexpr int NUM_MOTORS = 6;
+std::array<std::unique_ptr<ArmMotor>, NUM_MOTORS> motors;
 // ArmMotor *motors[numMotors];
 
 /**
  * @brief Defines space for all Joint references
  */
-const int numJoints = 5;
-std::array<std::unique_ptr<AbstractJoint>, numJoints> joints;
+constexpr int NUM_JOINTS = 5;
+std::array<std::unique_ptr<AbstractJoint>, NUM_JOINTS> joints;
 // AbstractJoint *joints[numJoints];
 /**
  * @brief The Joint State Publisher for MoveIt
@@ -110,7 +120,7 @@ bool configJointSetpoint(AbstractJoint* joint, int degreeIndex, double target, f
 void execute(const control_msgs::FollowJointTrajectoryGoalConstPtr& goal, Server* as) {
   if (!IKEnabled) {
     as->setAborted();
-    ROS_WARN("IK is disabled");
+    ROS_WARN("IK is disabled"); // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay, cppcoreguidelines-pro-type-vararg)
     return;
   }
 
@@ -164,14 +174,11 @@ void execute(const control_msgs::FollowJointTrajectoryGoalConstPtr& goal, Server
           IKEnabled = false;
           std_srvs::Trigger srv;
           if (enableServiceClient.call(srv)) {
-            ROS_WARN("%s", (std::string{"PLACEHOLDER_NAME: "} + srv.response.message).data());
+            ROS_WARN("%s", (std::string{"PLACEHOLDER_NAME: "} + srv.response.message).data()); // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay, cppcoreguidelines-pro-type-vararg)
           } else {
-            ROS_WARN("Error: failed to call service PLACEHOLDER_NAME");
+            ROS_WARN("Error: failed to call service PLACEHOLDER_NAME"); // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay, cppcoreguidelines-pro-type-vararg)
           }
           return;
-          // TODO: hand control back to driver
-          // standard service empty stdsrvs empty
-          // go into spin loop
         }
         jointIndex++;
       }
@@ -187,8 +194,8 @@ void execute(const control_msgs::FollowJointTrajectoryGoalConstPtr& goal, Server
 
   //When all positions have been reached, set the current task as succeeded
 
-  for(int i = 0; i < numMotors; i++){
-    motors.at(i)->setPower(0.0F);
+  for(auto &motor : motors){
+    motor->setPower(0.0F);
   }
   
   as->setSucceeded();
@@ -202,9 +209,9 @@ void publish(const ros::TimerEvent &event){
   std::vector<double> positions;
   sensor_msgs::JointState js_msg;
 
-  for (int i = 0; i < numMotors; i++){
-    names.push_back(motors.at(i)->getMotorName());
-    positions.push_back(motors.at(i)->getRads());
+  for (auto &motor : motors){
+    names.push_back(motor->getMotorName());
+    positions.push_back(motor->getRads());
   }
 
   positions[4] = positions[5] + positions[4] / 2;
@@ -235,6 +242,7 @@ int main(int argc, char** argv)
   pn.getParam("encoder_parameters", encParams);
 
   // Initialize all motors with their MoveIt name, WRoboclaw initialization, and reference to the current node
+  // TODO: fix
   motors.at(0) = new ArmMotor("elbow", 1, 0, static_cast<int>(encParams[0]["counts_per_rotation"]), static_cast<int>(encParams[0]["offset"]), n);
   motors[1] = new ArmMotor("forearm_roll", 1, 1, static_cast<int>(encParams[1]["counts_per_rotation"]), static_cast<int>(encParams[1]["offset"]), n);
   motors[2] = new ArmMotor("shoulder", 0, 1, static_cast<int>(encParams[2]["counts_per_rotation"]), static_cast<int>(encParams[2]["offset"]), n);
@@ -244,6 +252,7 @@ int main(int argc, char** argv)
   std::cout << "init motors" << std::endl;
 
   // Initialize all Joints
+  // TODO: fix
   joints.at(0) = new SimpleJoint(motors[0], &n);
   joints[1] = new SimpleJoint(motors[1], &n);
   joints[2] = new SimpleJoint(motors[2], &n);
@@ -256,7 +265,7 @@ int main(int argc, char** argv)
   joints[5] = temp;
   
   // Initialize the Joint State Data Publisher
-  jointStatePublisher = n.advertise<sensor_msgs::JointState>("/control/arm_joint_states", 1000);
+  jointStatePublisher = n.advertise<sensor_msgs::JointState>("/control/arm_joint_states", MESSAGE_CACHE_SIZE);
 
   // Initialize the Action Server
   Server server(n, "/arm_controller/follow_joint_trajectory", boost::bind(&execute, _1, &server), false);
@@ -264,14 +273,14 @@ int main(int argc, char** argv)
   server.start();
   std::cout << "server started" << std::endl;
 
-  ros::Timer timer = n.createTimer(ros::Duration(1.0 / 50.0), publish);
+  ros::Timer timer = n.createTimer(ros::Duration(TIMER_CALLBACK_DURATION), publish);
 
   enableServiceServer = n.advertiseService("start_IK", 
     static_cast<boost::function<bool(std_srvs::Trigger::Request&, std_srvs::Trigger::Response&)>>(
       [](std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &res)->bool{
         IKEnabled = true;
         res.message = "Arm IK Enabled";
-        res.success = true;
+        res.success = static_cast<unsigned char>(true);
         return true;
       }
     ));
