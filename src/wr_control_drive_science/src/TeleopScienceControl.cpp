dfce008cfb08a905409e840262594733f01f5c84
Ben Nowotny <bennowotny65535@gmail.com>
2022-04-27 18:01:12 -0500

    Modified ROS types to use message types instead of raw datatypes.

--- src/wr_control_drive_science/src/TeleopScienceControl.cpp
+++ src/wr_control_drive_science/src/TeleopScienceControl.cpp
@@ -3,6 +3,7 @@
 #include "std_msgs/Bool.h"
 #include "std_msgs/UInt32.h"
 #include "std_msgs/Float64.h"
+#include "std_msgs/Int16.h"
 
 constexpr std::uint32_t MESSAGE_CACHE_SIZE = 10;
 
@@ -11,46 +12,51 @@ int main(int argc, char** argv) {
 
     ros::NodeHandle n;
 
-    ros::Publisher screwLiftPow = n.advertise<int16_t>("/hsi/roboclaw/aux0/cmd/left", MESSAGE_CACHE_SIZE);
-    ros::Publisher turnTablePow = n.advertise<int16_t>("/hsi/roboclaw/aux0/cmd/right", MESSAGE_CACHE_SIZE);
-    ros::Publisher linearActuatorPow = n.advertise<int16_t>("/hsi/roboclaw/aux1/cmd/left", MESSAGE_CACHE_SIZE);
-    ros::Publisher clawPow = n.advertise<int16_t>("/hsi/roboclaw/aux0/cmd/right", MESSAGE_CACHE_SIZE);
-    ros::Publisher turnTableEncoder = n.advertise<_Float64>("pid/turnTable", MESSAGE_CACHE_SIZE);
+    ros::Publisher screwLiftPow = n.advertise<std_msgs::Int16>("/hsi/roboclaw/aux0/cmd/left", MESSAGE_CACHE_SIZE);
+    ros::Publisher turnTablePow = n.advertise<std_msgs::Int16>("/hsi/roboclaw/aux0/cmd/right", MESSAGE_CACHE_SIZE);
+    ros::Publisher linearActuatorPow = n.advertise<std_msgs::Int16>("/hsi/roboclaw/aux1/cmd/left", MESSAGE_CACHE_SIZE);
+    ros::Publisher clawPow = n.advertise<std_msgs::Int16>("/hsi/roboclaw/aux0/cmd/right", MESSAGE_CACHE_SIZE);
+    ros::Publisher turnTableEncoder = n.advertise<std_msgs::Float64>("pid/turnTable", MESSAGE_CACHE_SIZE);
 
     ros::Subscriber encoderSub = n.subscribe("/hsi/roboclaw/aux0/enc/right", MESSAGE_CACHE_SIZE,
             static_cast<boost::function<void(const std_msgs::UInt32::ConstPtr&)>>(
                 [&turnTableEncoder](const std_msgs::UInt32::ConstPtr& msg) {
-                    _Float64 ans = msg->data;
-                    turnTableEncoder.publish(ans);
+                    std_msgs::Float64 out;
+                    out.data = msg->data;
+                    turnTableEncoder.publish(out);
                 }
     ));
     ros::Subscriber screwLiftSub = n.subscribe("logic/science/screwLift", MESSAGE_CACHE_SIZE,
             static_cast<boost::function<void(const std_msgs::Float32::ConstPtr&)>>(
                 [&screwLiftPow](const std_msgs::Float32::ConstPtr& msg) {
-                    int16_t ans = msg->data * INT16_MAX;
-                    screwLiftPow.publish(ans);
+                    std_msgs::Int16 out;
+                    out.data = msg->data * INT16_MAX;
+                    screwLiftPow.publish(out);
                 }
     ));
     ros::Subscriber turnTableSub = n.subscribe("pid/turnTablePow", MESSAGE_CACHE_SIZE,
             static_cast<boost::function<void(const std_msgs::Float64::ConstPtr&)>>(
                 [&turnTablePow](const std_msgs::Float64::ConstPtr& msg) {
-                    int16_t ans = msg->data * INT16_MAX;
-                    turnTablePow.publish(ans);
+                    std_msgs::Int16 out;
+                    out.data = msg->data * INT16_MAX;
+                    turnTablePow.publish(out);
                 }
             )); 
             
     ros::Subscriber linearActuatorSub = n.subscribe("logic/science/linearActuator", MESSAGE_CACHE_SIZE,
             static_cast<boost::function<void(const std_msgs::Float32::ConstPtr&)>>(
                 [&linearActuatorPow](const std_msgs::Float32::ConstPtr& msg) {
-                    int16_t ans = msg->data * INT16_MAX;
-                    linearActuatorPow.publish(ans);
+                    std_msgs::Int16 out;
+                    out.data = msg->data * INT16_MAX;
+                    linearActuatorPow.publish(out);
                 }
     ));
     ros::Subscriber clawSub = n.subscribe("logic/science/claw", MESSAGE_CACHE_SIZE,
             static_cast<boost::function<void(const std_msgs::Float32::ConstPtr&)>>(
                 [&clawPow](const std_msgs::Float32::ConstPtr& msg) {
-                    int16_t ans = msg->data * INT16_MAX;
-                    clawPow.publish(ans);
+                    std_msgs::Int16 out;
+                    out.data = msg->data * INT16_MAX;
+                    clawPow.publish(out);
                 }
     ));
 
