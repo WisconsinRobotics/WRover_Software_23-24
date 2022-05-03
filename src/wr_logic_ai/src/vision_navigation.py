4e8353477993681573398b9f667d4cf5de6e4100
Colin Li <clikingofinfo@gmail.com>
2022-05-02 20:05:06 -0500

    Initial target area for 1m distance

--- src/wr_logic_ai/src/vision_navigation.py
+++ src/wr_logic_ai/src/vision_navigation.py
@@ -1,26 +1,30 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import UInt8
+from std_msgs.msg import UInt16
 from wr_logic_ai.msg import TargetMsg
 from wr_drive_msgs.msg import DriveTrainCmd
+
+TARGET_IN_1M_AREA = 40000
 
 drivetrain_topic = '/control/drive_system/cmd'
 target_topic = '/wr_logic_ai/shortrange_ai/vision_target_data'
 target_id_topic = '/wr_logic_ai/shortrange_ai/vision_target_id'
 drivetrain_pub = rospy.Publisher(drivetrain_topic, DriveTrainCmd, queue_size=1)
-target_id_pub = rospy.Publisher(target_id_topic, UInt8, queue_size=1)
+target_id_pub = rospy.Publisher(target_id_topic, UInt16, queue_size=1)
 current_post = 1
 
 
 def target_callback(data):
+    global current_post
     # TODO handle the gate (id 4 and 5)
     if data.id == current_post:
-        if at_post():
+        if at_post(data.area_estimate):
             # TODO change LED
             drivetrain_pub.publish(0, 0)
             current_post += 1
             target_id_pub.publish(current_post)
+            rospy.loginfo("Current post: %d", current_post)
         else:
             # TODO logic for driving to target
             drivetrain_pub.publish(0.1, 0.1)
@@ -32,7 +36,6 @@ def drive(left, right):
 
 
 def at_post():
-    # TODO tune using area of target to estimate distance from post
     return False
 
 
--- src/wr_logic_ai/src/vision_target_detection.py
+++ src/wr_logic_ai/src/vision_target_detection.py
@@ -4,13 +4,15 @@ import rospy
 import cv2 as cv
 import numpy as np
 import aruco_lib
-from std_msgs.msg import UInt8
+from std_msgs.msg import UInt16
 from wr_logic_ai.msg import TargetMsg
 from wr_drive_msgs.msg import DriveTrainCmd
 
 drivetrain_topic = '/control/drive_system/cmd'
+camera_width_topic = '/wr_logic_ai/shortrange_ai/vision_camera_width'
 target_topic = '/wr_logic_ai/shortrange_ai/vision_target_data'
 target_id_topic = '/wr_logic_ai/shortrange_ai/vision_target_id'
+camera_width_pub = rospy.Publisher(drivetrain_topic, UInt16, queue_size=1)
 drivetrain_pub = rospy.Publisher(drivetrain_topic, DriveTrainCmd, queue_size=1)
 current_ids = np.array(0)
 
@@ -29,9 +31,10 @@ def main():
     rospy.init_node('vision_target_detection', anonymous=True)
     rate = rospy.Rate(10)
 
-    rospy.Subscriber(target_id_topic, UInt8, target_id_callback)
+    rospy.Subscriber(target_id_topic, UInt16, target_id_callback)
     
     cap = cv.VideoCapture(0)
+    camera_width_pub.publish(cv.get(cv.CAP_PROP_FRAME_WIDTH))
 
     if not cap.isOpened():
         print('Cannot open camera')
@@ -53,9 +56,10 @@ def main():
             bottom_right_arr = current_target[2].tolist()
             side_lengths = []
             for i in range(len(current_target)):
-                side_lengths.append(np.linalg.norm(current_target[i] - current_target[i+1]))
+               side_lengths.append(np.linalg.norm(current_target[i-1] - current_target[i]))
             area_estimate = max(side_lengths)**2
-            rospy.loginfo(id, top_left_arr, bottom_right_arr, area_estimate)
+            rospy.loginfo("Target ID: %s, top left corner: %s, bottom right corner: %s, corners: %s, side_lengths: %s, area_estimate: %f", 
+                str(id), str(top_left_arr), str(bottom_right_arr), str(current_target.tolist()), str(side_lengths), area_estimate)
             target_pub.publish(id, top_left_arr, bottom_right_arr, area_estimate)
 
         rate.sleep()
