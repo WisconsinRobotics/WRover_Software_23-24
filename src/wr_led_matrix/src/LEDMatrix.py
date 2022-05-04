41ae2cc35da28e8c0f82147927923c188ee9e0f7
kuh <kohlerzuehlke@gmail.com>
2022-05-03 19:22:45 -0500

    LED panel ROS

--- src/wr_led_matrix/src/LEDMatrix.py
+++ src/wr_led_matrix/src/LEDMatrix.py
@@ -3,16 +3,18 @@
 #TODO: Import req'd things (rospy, serial, custom service, ...)
 import rospy
 import serial
-from wr_led_matrix.srv import LEDMatrix
+from wr_led_matrix.srv import LEDMatrix,LEDMatrixRequest,LEDMatrixResponse
 
 #TODO: Define a callback function that that takes in the service request and reply objects and writes to the service reply if you chose to use that optional reply data.  Please use type hints
-def handle_leds(req):
+def handle_leds(req: LEDMatrixRequest):
     p = bytearray()
     p.append(req.RED)
     p.append(req.GREEN)
     p.append(req.BLUE)
     with serial.Serial('/dev/ttyUSB0', 9600) as s:
         s.write(p)
+    
+    return LEDMatrixResponse()
     
 #Main executable
 if __name__=="__main__":
