8c1dbf3578a51f9a07ecb030d18a1ffe463fb23d
Colin Li <clikingofinfo@gmail.com>
2022-05-05 18:47:00 -0500

    refactor code to use a service

--- src/wr_logic_ai/src/vision_navigation.py
+++ src/wr_logic_ai/src/vision_navigation.py
@@ -2,7 +2,7 @@
 
 import rospy
 from std_msgs.msg import UInt16
-from wr_logic_ai.msg import TargetMsg
+from wr_logic_ai.srv import GetTargetInfo, GetTargetInfoResponse
 from wr_drive_msgs.msg import DriveTrainCmd
 
 CAMERA_WIDTH = 1280
@@ -11,32 +11,27 @@ DRIVE_SPEED = 0.1
 TURN_kP = 0.0001
 
 drivetrain_topic = '/control/drive_system/cmd'
-target_topic = '/wr_logic_ai/shortrange_ai/vision_target_data'
-target_id_topic = '/wr_logic_ai/shortrange_ai/vision_target_id'
 drivetrain_pub = rospy.Publisher(drivetrain_topic, DriveTrainCmd, queue_size=1)
-target_id_pub = rospy.Publisher(target_id_topic, UInt16, queue_size=1)
 current_post = 0
 
 
-def target_callback(data):
-    global current_post
-    # TODO handle the gate (id 4 and 5)
-    if data.id == current_post:
-        if at_post(data.area):
-            # TODO change LED
-            rospy.loginfo("Reached post, id: %d", current_post)
-            drivetrain_pub.publish(0, 0)
-            current_post += 1
-            target_id_pub.publish(current_post)
-        else:
-            # TODO logic for driving through gate (ids 4 and 5)
-            x_offset = CAMERA_WIDTH/2 - data.x_center
-            turn_speed = TURN_kP * x_offset
-            left_speed = min(max(DRIVE_SPEED, DRIVE_SPEED - turn_speed), 1)
-            right_speed = min(max(DRIVE_SPEED, DRIVE_SPEED + turn_speed), 1)
-            rospy.loginfo("left speed: %f, right speed: %f", left_speed, right_speed)
-            drivetrain_pub.publish(left_speed, right_speed)
-    rospy.loginfo(str(data))
+# def target_callback(data):
+#     global current_post
+#     if data.id == current_post:
+#         if at_post(data.area):
+#             # TODO change LED
+#             rospy.loginfo("Reached post, id: %d", current_post)
+#             drivetrain_pub.publish(0, 0)
+#             current_post += 1
+#         else:
+#             # TODO logic for driving through gate (ids 4 and 5)
+#             x_offset = CAMERA_WIDTH/2 - data.x_center
+#             turn_speed = TURN_kP * x_offset
+#             left_speed = min(max(DRIVE_SPEED, DRIVE_SPEED - turn_speed), 1)
+#             right_speed = min(max(DRIVE_SPEED, DRIVE_SPEED + turn_speed), 1)
+#             rospy.loginfo("left speed: %f, right speed: %f", left_speed, right_speed)
+#             drivetrain_pub.publish(left_speed, right_speed)
+#     rospy.loginfo(str(data))
 
 
 def drive(left, right):
@@ -48,12 +43,37 @@ def at_post(area):
 
 
 def main():
+    global current_post
+
     rospy.init_node('vision_navigation', anonymous=True)
     rate = rospy.Rate(10)
+    
+    rospy.wait_for_service('get_target_info')
+    get_target_info = rospy.ServiceProxy('get_target_info', GetTargetInfo)
 
-    target_id_pub.publish(current_post)
-    rospy.Subscriber(target_topic, TargetMsg, target_callback)
-    rospy.spin()
+    while not rospy.is_shutdown():
+        try:
+            target_info = get_target_info(current_post)
+            if target_info.is_visible:                
+                if at_post(target_info.area):
+                    # TODO change LED
+                    rospy.loginfo("Reached post, id: %d", current_post)
+                    drive(0, 0)
+                    current_post += 1
+                else:
+#                   # TODO logic for driving through gate (ids 4 and 5)
+                    x_offset = CAMERA_WIDTH/2 - target_info.x_center
+                    turn_speed = TURN_kP * x_offset
+                    left_speed = min(max(DRIVE_SPEED, DRIVE_SPEED - turn_speed), 1)
+                    right_speed = min(max(DRIVE_SPEED, DRIVE_SPEED + turn_speed), 1)
+                    rospy.loginfo("left speed: %f, right speed: %f", left_speed, right_speed)
+                    drive(left_speed, right_speed)
+            else:
+                drive(-0.1, 0.1)
+        except rospy.ServiceException as e:
+            rospy.logerr("Service call failed: %s", e)
+
+        rate.sleep()
 
 
 if __name__ == "__main__":
--- src/wr_logic_ai/src/vision_target_detection.py
+++ src/wr_logic_ai/src/vision_target_detection.py
@@ -5,44 +5,59 @@ import cv2 as cv
 import numpy as np
 import aruco_lib
 from std_msgs.msg import UInt16
-from wr_logic_ai.msg import TargetMsg
+# from wr_logic_ai.msg import TargetMsg
+from wr_logic_ai.srv import GetTargetInfo, GetTargetInfoResponse
+from wr_logic_ai.msg import CameraInfoMsg
 from wr_logic_ai.msg import CameraInfoMsg
 from wr_drive_msgs.msg import DriveTrainCmd
 
-drivetrain_topic = '/control/drive_system/cmd'
+# target_topic = '/wr_logic_ai/shortrange_ai/vision_target_data'
+# target_id_topic = '/wr_logic_ai/shortrange_ai/vision_target_id'
 camera_info_topic = '/wr_logic_ai/shortrange_ai/vision_camera_info'
-target_topic = '/wr_logic_ai/shortrange_ai/vision_target_data'
-target_id_topic = '/wr_logic_ai/shortrange_ai/vision_target_id'
 camera_info = rospy.Publisher(camera_info_topic, CameraInfoMsg, queue_size=1)
-drivetrain_pub = rospy.Publisher(drivetrain_topic, DriveTrainCmd, queue_size=1)
-current_ids = np.array(0)
+
+ids = np.empty(0)
+corners = np.empty(0)
 
 
-def target_id_callback(id):
-    global current_ids
-    if id not in current_ids:
-        rotate(0.1)
+def handle_get_target_info(req):
+    if req.id not in ids:
+        return GetTargetInfoResponse(False, [0,0], [0,0], 0, 0)
+    else:
+        id_loc = np.where(ids == req.id)
+        target = corners[id_loc[0]][0]
+        top_left_arr = target[0].tolist()
+        bottom_right_arr = target[2].tolist()
+        side_lengths = []
+        min_x = target[0][0]
+        max_x = target[0][0]
+        for i in range(len(target)):
+            side_lengths.append(np.linalg.norm(target[i-1] - target[i]))
+            min_x = min(min_x, target[i][0])
+            max_x = max(max_x, target[i][0])
+        x_center = (max_x - min_x) / 2
+        area_estimate = max(side_lengths)**2
 
-
-def rotate(speed):
-    drivetrain_pub.publish(speed, -speed)
+        rospy.loginfo("Target ID: %s, top left corner: %s, bottom right corner: %s, corners: %s, side_lengths: %s, area_estimate: %f", 
+            str(id), str(top_left_arr), str(bottom_right_arr), str(target.tolist()), str(side_lengths), area_estimate)
+        return GetTargetInfoResponse(True, top_left_arr, bottom_right_arr, x_center, area_estimate)
 
 
 def main():
-    global current_ids
+    global ids, corners
 
-    target_pub = rospy.Publisher(target_topic, TargetMsg, queue_size=10)
-    rospy.init_node('vision_target_detection', anonymous=True)
+    rospy.init_node('vision_target_detection')
+    target_service = rospy.Service('get_target_info', GetTargetInfo, handle_get_target_info)
+
     rate = rospy.Rate(10)
 
-    stream_url = rospy.get_param('~video_stream')
+    stream_url = rospy.get_param('video_stream')
+    rospy.loginfo("Stream: " + stream_url)
 
-    rospy.Subscriber(target_id_topic, UInt16, target_id_callback)
-
-    if stream_url != None or stream_url != '':
-        cap = cv.VideoCapture(stream_url)
-    else:
-        cap = cv.VideoCapture(0)
+    # if stream_url != None or stream_url != '':
+        # cap = cv.VideoCapture(stream_url)
+    # else:
+    cap = cv.VideoCapture(0)
 
     camera_info.publish(cap.get(cv.CAP_PROP_FRAME_WIDTH), cap.get(cv.CAP_PROP_FRAME_HEIGHT))
 
@@ -50,6 +65,7 @@ def main():
         print('Cannot open camera')
         exit()
 
+    
     while cap.isOpened() and not rospy.is_shutdown():
         ret, frame = cap.read()
 
@@ -58,25 +74,24 @@ def main():
             break
 
         (corners, ids, rejected) = aruco_lib.detect_markers(frame)
-        current_ids = ids
 
-        for i in range(len(corners)):
-            id = ids[i].item()
-            current_target = corners[i][0]
-            top_left_arr = current_target[0].tolist()
-            bottom_right_arr = current_target[2].tolist()
-            side_lengths = []
-            min_x = current_target[0][0]
-            max_x = current_target[0][0]
-            for i in range(len(current_target)):
-               side_lengths.append(np.linalg.norm(current_target[i-1] - current_target[i]))
-               min_x = min(min_x, current_target[i][0])
-               max_x = max(max_x, current_target[i][0])
-            x_center = (max_x - min_x) / 2
-            area_estimate = max(side_lengths)**2
-            rospy.loginfo("Target ID: %s, top left corner: %s, bottom right corner: %s, corners: %s, side_lengths: %s, area_estimate: %f", 
-                str(id), str(top_left_arr), str(bottom_right_arr), str(current_target.tolist()), str(side_lengths), area_estimate)
-            target_pub.publish(id, top_left_arr, bottom_right_arr, x_center, area_estimate)
+    #     for i in range(len(corners)):
+    #         id = ids[i].item()
+    #         current_target = corners[i][0]
+    #         top_left_arr = current_target[0].tolist()
+    #         bottom_right_arr = current_target[2].tolist()
+    #         side_lengths = []
+    #         min_x = current_target[0][0]
+    #         max_x = current_target[0][0]
+    #         for i in range(len(current_target)):
+    #            side_lengths.append(np.linalg.norm(current_target[i-1] - current_target[i]))
+    #            min_x = min(min_x, current_target[i][0])
+    #            max_x = max(max_x, current_target[i][0])
+    #         x_center = (max_x - min_x) / 2
+    #         area_estimate = max(side_lengths)**2
+    #         rospy.loginfo("Target ID: %s, top left corner: %s, bottom right corner: %s, corners: %s, side_lengths: %s, area_estimate: %f", 
+    #             str(id), str(top_left_arr), str(bottom_right_arr), str(current_target.tolist()), str(side_lengths), area_estimate)
+    #         target_pub.publish(id, top_left_arr, bottom_right_arr, x_center, area_estimate)
 
         rate.sleep()
 
