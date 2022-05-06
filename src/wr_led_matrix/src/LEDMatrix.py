467f7e9aa8c3e3f0be1cde224468b28a9807337c
kuh <kohlerzuehlke@gmail.com>
2022-05-05 19:13:40 -0500

    Finished ROS param info and added arduino code

--- src/wr_led_matrix/src/LEDMatrix.py
+++ src/wr_led_matrix/src/LEDMatrix.py
@@ -3,24 +3,34 @@
 #TODO: Import req'd things (rospy, serial, custom service, ...)
 import rospy
 import serial
-import time
 from wr_led_matrix.srv import LEDMatrix,LEDMatrixRequest,LEDMatrixResponse
 
 s: serial.Serial
+baud = rospy.get_param("/led_matrix/baud")
+port = rospy.get_param("/led_matrix/com_port")
+
 
 #TODO: Define a callback function that that takes in the service request and reply objects and writes to the service reply if you chose to use that optional reply data.  Please use type hints
 def handle_leds(req: LEDMatrixRequest):
-    p = bytearray([req.RED, req.GREEN, req.BLUE])
-    s.write(p)
+    """Send led color to matrix via serial
+
+    Args:
+        req (LEDMatrixRequest): _description_
+
+    Returns:
+        null
+    """
+    packet = bytearray([req.RED, req.GREEN, req.BLUE])
+    s.write(packet)
     return LEDMatrixResponse()
-    
+
 #Main executable
 if __name__=="__main__":
-    #TODO: Setup the ROS service
     rospy.init_node("LEDMatrix")
     rospy.Service('LEDMatrix', LEDMatrix, handle_leds)
 
-    with serial.Serial('/dev/ttyUSB1', 9600) as s:
+    with serial.Serial(port, baud) as s:
         #ROS Spin indefinitely (the heavy lifting is in the service callback)
+        rospy.loginfo("Serial port open, starting spin")
         rospy.spin()
         s.write(bytearray([0,0,0])) #Shut off LED panel
