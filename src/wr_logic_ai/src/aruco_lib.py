8a349286575ca6975765e035994a36ee6c3146a9
Colin Li <clikingofinfo@gmail.com>
2023-02-13 20:18:50 -0600

    Update aruco lib for opencv 4.7

--- src/wr_logic_ai/src/aruco_lib.py
+++ src/wr_logic_ai/src/aruco_lib.py
@@ -1,29 +1,40 @@
 import numpy as np
 import cv2 as cv
 
-ARUCO_DICT = cv.aruco.Dictionary_get(cv.aruco.DICT_4X4_250)
-ARUCO_PARAMS = cv.aruco.DetectorParameters_create()
+# ARUCO_DICT = cv.aruco.Dictionary_get(cv.aruco.DICT_4X4_250)
+# ARUCO_PARAMS = cv.aruco.DetectorParameters_create()
 
 
-def generate_charuco(board_width, board_height):
-    return cv.aruco.CharucoBoard_create(board_width, board_height, 0.04, 0.02, ARUCO_DICT)
+# def generate_charuco(board_width, board_height):
+#     return cv.aruco.CharucoBoard_create(board_width, board_height, 0.04, 0.02, ARUCO_DICT)
 
 
-def detect_markers(img):
-    return cv.aruco.detectMarkers(img, ARUCO_DICT, parameters=ARUCO_PARAMS)
+# def detect_markers(img):
+#     return cv.aruco.detectMarkers(img, ARUCO_DICT, parameters=ARUCO_PARAMS)
 
 
-def draw_markers(img):
-    imgDetected = img.copy()
-    imgRejected = img.copy()
-    (corners, ids, rejected) = detect_markers(img)
-    cv.aruco.drawDetectedMarkers(imgDetected, corners, ids)
-    cv.aruco.drawDetectedMarkers(
-        imgRejected, rejected, borderColor=(100, 0, 240))
-    return (imgDetected, imgRejected)
+# def draw_markers(img):
+#     imgDetected = img.copy()
+#     imgRejected = img.copy()
+#     (corners, ids, rejected) = detect_markers(img)
+#     cv.aruco.drawDetectedMarkers(imgDetected, corners, ids)
+#     cv.aruco.drawDetectedMarkers(
+#         imgRejected, rejected, borderColor=(100, 0, 240))
+#     return (imgDetected, imgRejected)
 
 
-def set_params(params):
-    if params.getNode('adaptiveThreshConstant') != None:
-        ARUCO_PARAMS.adaptiveThreshConstant = float(
-            params.getNode('adaptiveThreshConstant'))
+# def set_params(params):
+#     if params.getNode('adaptiveThreshConstant') != None:
+#         ARUCO_PARAMS.adaptiveThreshConstant = float(
+#             params.getNode('adaptiveThreshConstant'))
+
+
+ARUCO_DICT = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_50)
+
+
+def detect_aruco(img: np.ndarray):
+    return cv.aruco.ArucoDetector(ARUCO_DICT).detectMarkers(img)
+
+
+def detect_contours(img: np.ndarray):
+    pass
--- src/wr_logic_ai/src/vision_target_detection.py
+++ src/wr_logic_ai/src/vision_target_detection.py
@@ -4,55 +4,60 @@ import rospy
 import cv2 as cv
 import numpy as np
 import aruco_lib
-from std_msgs.msg import UInt16
+# from std_msgs.msg import UInt16
 # from wr_logic_ai.msg import TargetMsg
 from wr_logic_ai.srv import GetTargetInfo, GetTargetInfoResponse
-from wr_logic_ai.msg import CameraInfoMsg
-from wr_logic_ai.msg import CameraInfoMsg
+# from wr_logic_ai.msg import CameraInfoMsg
+# from wr_logic_ai.msg import CameraInfoMsg
 
 # target_topic = '/wr_logic_ai/shortrange_ai/vision_target_data'
 # target_id_topic = '/wr_logic_ai/shortrange_ai/vision_target_id'
-camera_info_topic = '/wr_logic_ai/shortrange_ai/vision_camera_info'
-camera_info = rospy.Publisher(camera_info_topic, CameraInfoMsg, queue_size=1)
+# camera_info_topic = '/wr_logic_ai/shortrange_ai/vision_camera_info'
+# camera_info = rospy.Publisher(camera_info_topic, CameraInfoMsg, queue_size=1)
 
-ids = np.empty(0)
-corners = np.empty(0)
+# ids = np.empty(0)
+# corners = np.empty(0)
+cap: cv.VideoCapture = None
 
 
 def handle_get_target_info(req: GetTargetInfo):
-    if len(corners) == 0:
-        return GetTargetInfoResponse(False, [0, 0], [0, 0], 0, 0)
+    ret, frame = cap.read()
 
-    id_list = ids.flatten().tolist()
-    if req.id not in id_list:
-        return GetTargetInfoResponse(False, [0, 0], [0, 0], 0, 0)
+    if not ret:
+        rospy.logerr('Failed to read frame')
+    else:
+        (corners, ids, _) = aruco_lib.detect_aruco(frame)
+        if len(ids) != 0:
+            id_list = ids.flatten().tolist()
+ 
+            try:
+                id_ind = id_list.index(req.id)
+                target = corners[id_ind][0]
+                top_left_arr = target[0].tolist()
+                bottom_right_arr = target[2].tolist()
+                side_lengths = []
+                min_x = target[0][0]
+                max_x = target[0][0]
+                for i in range(len(target)):
+                    side_lengths.append(np.linalg.norm(target[i-1] - target[i]))
+                    min_x = min(min_x, target[i][0])
+                    max_x = max(max_x, target[i][0])
+                x_center = (max_x - min_x) / 2
+                area_estimate = max(side_lengths)**2
 
-    id_ind = id_list.index(req.id)
-    target = corners[id_ind][0]
-    top_left_arr = target[0].tolist()
-    bottom_right_arr = target[2].tolist()
-    side_lengths = []
-    min_x = target[0][0]
-    max_x = target[0][0]
-    for i in range(len(target)):
-        side_lengths.append(np.linalg.norm(target[i-1] - target[i]))
-        min_x = min(min_x, target[i][0])
-        max_x = max(max_x, target[i][0])
-    x_center = (max_x - min_x) / 2
-    area_estimate = max(side_lengths)**2
-
-    rospy.loginfo("Target ID: %s, top left corner: %s, bottom right corner: %s, corners: %s, side_lengths: %s, area_estimate: %f",
-                  str(id), str(top_left_arr), str(bottom_right_arr), str(target.tolist()), str(side_lengths), area_estimate)
-    return GetTargetInfoResponse(True, top_left_arr, bottom_right_arr, x_center, area_estimate)
+                rospy.loginfo("Target ID: %s, top left corner: %s, bottom right corner: %s, corners: %s, side_lengths: %s, area_estimate: %f",
+                            str(id), str(top_left_arr), str(bottom_right_arr), str(target.tolist()), str(side_lengths), area_estimate)
+                return GetTargetInfoResponse(True, top_left_arr, bottom_right_arr, x_center, area_estimate)
+            except:
+                rospy.loginfo("Requested target not found")
+    return GetTargetInfoResponse(False, [0, 0], [0, 0], 0, 0)
 
 
 def main():
-    global ids, corners
+    global cap
 
     rospy.init_node('vision_target_detection')
     rospy.Service('get_target_info', GetTargetInfo, handle_get_target_info)
-
-    rate = rospy.Rate(10)
 
     stream_url = rospy.get_param('video_stream')
     rospy.loginfo("Stream: " + stream_url)
@@ -62,42 +67,11 @@ def main():
     else:
         cap = cv.VideoCapture(0)
 
-    camera_info.publish(cap.get(cv.CAP_PROP_FRAME_WIDTH),
-                        cap.get(cv.CAP_PROP_FRAME_HEIGHT))
-
     if not cap.isOpened():
-        rospy.logerr('Cannot open camera')
+        rospy.logerr('Failed to open camera')
         exit()
 
-    while cap.isOpened() and not rospy.is_shutdown():
-        ret, frame = cap.read()
-
-        if not ret:
-            rospy.logdebug('Cannot read frame')
-            break
-
-        (corners, ids, rejected) = aruco_lib.detect_markers(frame)
-
-    #     for i in range(len(corners)):
-    #         id = ids[i].item()
-    #         current_target = corners[i][0]
-    #         top_left_arr = current_target[0].tolist()
-    #         bottom_right_arr = current_target[2].tolist()
-    #         side_lengths = []
-    #         min_x = current_target[0][0]
-    #         max_x = current_target[0][0]
-    #         for i in range(len(current_target)):
-    #            side_lengths.append(np.linalg.norm(current_target[i-1] - current_target[i]))
-    #            min_x = min(min_x, current_target[i][0])
-    #            max_x = max(max_x, current_target[i][0])
-    #         x_center = (max_x - min_x) / 2
-    #         area_estimate = max(side_lengths)**2
-    #         rospy.loginfo("Target ID: %s, top left corner: %s, bottom right corner: %s, corners: %s, side_lengths: %s, area_estimate: %f",
-    #             str(id), str(top_left_arr), str(bottom_right_arr), str(current_target.tolist()), str(side_lengths), area_estimate)
-    #         target_pub.publish(id, top_left_arr, bottom_right_arr, x_center, area_estimate)
-
-        rate.sleep()
-
+    rospy.spin()
     cap.release()
 
 
