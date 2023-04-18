0ae8d85ab3f27ecd763062c125612afbb8cc4ecb
WR-BaseStation <wisconsinrobotics@cae.wisc.edu>
2023-04-17 21:00:45 -0500

    more data

--- src/wr_logic_ai/nodes/hardware_testing.py
+++ src/wr_logic_ai/nodes/hardware_testing.py
@@ -3,7 +3,7 @@
 import rospy
 from BNO055 import BNO055
 from adafruit_gps import I2C,GPS_GtopI2C as GPS
-from std_msgs.msg import Float32
+from std_msgs.msg import Int32
 import time
 
 ## Constants to vary
@@ -11,7 +11,9 @@ f = 100     # Hz | Rate at which pose messages are published
 
 rospy.init_node('ai_hardware_testing', anonymous=False)
 rate = rospy.Rate(f)
-test_pub_heading = rospy.Publisher('/test_heading_data', Float32, queue_size=1)
+pub_x = rospy.Publisher('/mag_x', Int32, queue_size=1)
+pub_y = rospy.Publisher('/mag_y', Int32, queue_size=1)
+pub_z = rospy.Publisher('/mag_z', Int32, queue_size=1)
 
 # Create IMU Sensor object
 sensor = BNO055()
@@ -41,18 +43,20 @@ def get_heading():
 def publish_heading():
     test_pub_heading.publish(get_heading())
 
+def cb():
+    mag_z = int.from_bytes(sensor.readBytes(BNO055.BNO055_MAG_DATA_Z_LSB_ADDR, 2), 'little')
+    mag_x = int.from_bytes(sensor.readBytes(BNO055.BNO055_MAG_DATA_X_LSB_ADDR, 2), 'little')
+    mag_y = int.from_bytes(sensor.readBytes(BNO055.BNO055_MAG_DATA_Y_LSB_ADDR, 2), 'little')
+
+    mag_x = mag_x if mag_x < 32768 else mag_x-65536
+    mag_y = mag_y if mag_y < 32768 else mag_y-65536
+    mag_z = mag_z if mag_z < 32768 else mag_z-65536
+
+    pub_x.publish(mag_x)
+    pub_y.publish(mag_y)
+    pub_z.publish(mag_z)
+
 if __name__ == '__main__':
-    while True:
-        mag_z = int.from_bytes(sensor.readBytes(BNO055.BNO055_MAG_DATA_Z_LSB_ADDR, 2), 'little')
-        mag_x = int.from_bytes(sensor.readBytes(BNO055.BNO055_MAG_DATA_X_LSB_ADDR, 2), 'little')
-        mag_y = int.from_bytes(sensor.readBytes(BNO055.BNO055_MAG_DATA_Y_LSB_ADDR, 2), 'little')
-
-        mag_x = mag_x if mag_x < 32768 else mag_x-65536
-        mag_y = mag_y if mag_y < 32768 else mag_y-65536
-        mag_z = mag_z if mag_z < 32768 else mag_z-65536
-
-        print(f"x={mag_x}")
-        print(f"y={mag_y}")
-        print(f"z={mag_z}")
-
-        time.sleep(1)
+    timer = rospy.Timer(rate.sleep_dur, lambda _: cb())
+    timer.start()
+    rospy.spin()
