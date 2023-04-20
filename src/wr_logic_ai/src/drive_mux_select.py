e25902405a6cfa9708a2926a58cf17eb745879c4
Sungkar <worldbeyound2022@gmail.com>
2023-04-19 20:39:52 -0500

    Made class

--- src/wr_logic_ai/src/drive_mux_select.py
+++ src/wr_logic_ai/src/drive_mux_select.py
@@ -5,14 +5,20 @@ from std_msgs.msg import String
 from typing import Dict
 from topic_tools.srv import MuxSelect
 
+current = -1
+class MUX:
+    def __init__(self) -> None:
+        self.last_message = None
 
-def state_call_back(message: NavigationState, conversion_table: Dict[int, str], mux_name : str):
-    rospy.wait_for_service(f"/{mux_name}/select")
-    try:
-        proxy = rospy.ServiceProxy(f"/{mux_name}/select", MuxSelect)
-        proxy(conversion_table[message.state])
-    except rospy.ServiceException as e:
-        rospy.logerr(e)
+    def state_call_back(self, message: NavigationState, conversion_table: Dict[int, str], mux_name : str):
+        if (self.last_message != message):
+            self.last_message = message
+            rospy.wait_for_service(f"/{mux_name}/select")
+            try:
+                proxy = rospy.ServiceProxy(f"/{mux_name}/select", MuxSelect)
+                proxy(conversion_table[message.state])
+            except rospy.ServiceException as e:
+                rospy.logerr(e)
 
 if __name__ == '__main__':
     rospy.init_node('mux_select_node')
@@ -20,7 +26,6 @@ if __name__ == '__main__':
         NavigationState.NAVIGATION_STATE_LONG_RANGE: rospy.get_param("~long_range_topic_name"),
         NavigationState.NAVIGATION_STATE_SHORT_RANGE: rospy.get_param("~short_range_topic_name")
     }
-    
-    sub = rospy.Subscriber("/navigation_state",
-                           NavigationState, lambda msg: state_call_back(msg, conversion, rospy.get_param("~mux_name")))
+    mux_instance = MUX()
+    sub = rospy.Subscriber("/navigation_state", NavigationState, lambda msg: mux_instance.state_call_back(msg, conversion, rospy.get_param("~mux_name")))
     rospy.spin()
