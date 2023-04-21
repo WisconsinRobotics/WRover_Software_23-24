1e1ec32f8f72b0e0ff9bca7e9d7004dd2afb37d2
WR-BaseStation <wisconsinrobotics@cae.wisc.edu>
2023-04-20 19:58:34 -0500

    adding moving average window

--- src/wr_hsi_sensing/nodes/imu_testing.py
+++ src/wr_hsi_sensing/nodes/imu_testing.py
@@ -20,12 +20,27 @@ pub_heading = rospy.Publisher('/heading', Float64, queue_size=1)
 
 MAG_NOISE_THRESH = 1000
 
+class MovingAverage:
+    _AVERAGING_WINDOW_SIZE = 3
+    def __init__(self):
+        self._values = []
+
+    def feed(self, value):
+        self._values.insert(0, value)
+        if len(self._values) > MovingAverage._AVERAGING_WINDOW_SIZE:
+            self._values.pop()
+    
+    def get_value(self) -> float:
+        return sum(self._values)/len(self._values) if len(self._values) > 0 else 0
+
 min_x = float('inf')
 max_x = float('-inf')
 min_y = float('inf')
 max_y = float('-inf')
 prev_x = 0
 prev_y = 0
+x_filter = MovingAverage()
+y_filter = MovingAverage()
 
 # Create IMU Sensor object
 sensor = BNO055()
@@ -55,6 +70,8 @@ def cb():
     global min_y
     global prev_x
     global prev_y
+    global x_filter
+    global y_filter
 
     mag_z = int.from_bytes(sensor.readBytes(BNO055.BNO055_MAG_DATA_Z_LSB_ADDR, 2), 'little')
     mag_x = int.from_bytes(sensor.readBytes(BNO055.BNO055_MAG_DATA_X_LSB_ADDR, 2), 'little')
@@ -64,24 +81,32 @@ def cb():
     mag_y = mag_y if mag_y < 32768 else mag_y-65536
     mag_z = mag_z if mag_z < 32768 else mag_z-65536
 
+    # TODO: Consider differential noise filtering (prev_msg) or absolute
+    if abs(mag_x - prev_x) < MAG_NOISE_THRESH:
+        x_filter.feed(mag_x)
+
+    if abs(mag_y - prev_y) < MAG_NOISE_THRESH:
+        y_filter.feed(mag_y)
+
     print(f"min/max x: {min_x}, {max_x}")
 
-    # TODO: Consider differential noise filtering (prev_msg) or absolute
-    if abs(mag_x - prev_x) < MAG_NOISE_THRESH and abs(mag_y - prev_y) < MAG_NOISE_THRESH:
-        max_x = max(max_x, mag_x)
-        min_x = min(min_x, mag_x)
-        max_y = max(max_y, mag_y)
-        min_y = min(min_y, mag_y)
-        prev_x = mag_x
-        prev_y = mag_y
+    mag_x = x_filter.get_value()
+    mag_y = y_filter.get_value()
+    
+    max_x = max(max_x, mag_x)
+    min_x = min(min_x, mag_x)
+    max_y = max(max_y, mag_y)
+    min_y = min(min_y, mag_y)
+    prev_x = mag_x
+    prev_y = mag_y
 
-        norm_x = mag_x - ((max_x - min_x) / 2 + min_x)
-        norm_y = mag_y - ((max_y - min_y) / 2 + min_y)
-        pub_norm_x.publish(int(norm_x))
-        pub_norm_y.publish(int(norm_y))
+    norm_x = mag_x - ((max_x - min_x) / 2 + min_x)
+    norm_y = mag_y - ((max_y - min_y) / 2 + min_y)
+    pub_norm_x.publish(int(norm_x))
+    pub_norm_y.publish(int(norm_y))
 
-        heading = math.atan2(norm_y, norm_x)
-        pub_heading.publish(Float64(heading))
+    heading = math.atan2(norm_y, norm_x)
+    pub_heading.publish(Float64(heading))
 
     mag_z = mag_z if abs(mag_z) < 10000 else 0
 
