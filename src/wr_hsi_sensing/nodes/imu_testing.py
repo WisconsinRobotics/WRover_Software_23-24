2f56f232af0f1a891d90ca9500ff09e309bf650b
WR-BaseStation <wisconsinrobotics@cae.wisc.edu>
2023-04-20 19:08:37 -0500

    debug normalization

--- src/wr_hsi_sensing/nodes/imu_testing.py
+++ src/wr_hsi_sensing/nodes/imu_testing.py
@@ -17,10 +17,14 @@ pub_z = rospy.Publisher('/mag_z', Int32, queue_size=1)
 pub_norm_x = rospy.Publisher('/norm_mag_x', Int32, queue_size=1)
 pub_norm_y = rospy.Publisher('/norm_mag_y', Int32, queue_size=1)
 
+MAG_NOISE_THRESH = 100
+
 min_x = float('inf')
 max_x = float('-inf')
 min_y = float('inf')
 max_y = float('-inf')
+prev_x = 0
+prev_y = 0
 
 # Create IMU Sensor object
 sensor = BNO055()
@@ -57,12 +61,13 @@ def cb():
     mag_y = mag_y if mag_y < 32768 else mag_y-65536
     mag_z = mag_z if mag_z < 32768 else mag_z-65536
 
-    # assuming that absolute readings are always less than 10000
-    if abs(mag_x) < 10000 and abs(mag_y) < 10000:
+    if abs(mag_x - prev_x) < MAG_NOISE_THRESH and abs(mag_y - prev_y) < MAG_NOISE_THRESH:
         max_x = max(max_x, mag_x)
         min_x = min(min_x, mag_x)
         max_y = max(max_y, mag_y)
         min_y = min(min_y, mag_y)
+        prev_x = mag_x
+        prev_y = mag_y
 
         norm_x = mag_x - (max_x - min_x) / 2
         norm_y = mag_y - (max_y - min_y) / 2
