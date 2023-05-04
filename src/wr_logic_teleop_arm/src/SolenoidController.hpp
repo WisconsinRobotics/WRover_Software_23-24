8edc4f19a871643aa846af5f7632ff7a55fce58e
Rus Milovanovic <cmilovanovic@wisc.edu>
2023-05-03 20:02:44 -0500

    Made extendSolenoid private.

--- src/wr_logic_teleop_arm/src/SolenoidController.hpp
+++ src/wr_logic_teleop_arm/src/SolenoidController.hpp
@@ -9,8 +9,6 @@
 class SolenoidController {
 public:
     explicit SolenoidController(ros::NodeHandle&);
-
-    void extendSolenoid(const std_msgs::Bool::ConstPtr& msg);
     
     ~SolenoidController();
     SolenoidController(const SolenoidController&) = delete;
@@ -23,10 +21,13 @@ private:
     bool yPressed;
 
     std::ofstream file;
+    std::ofstream fileExport;
     std::ofstream fileUnexport;
 
     static constexpr uint32_t MESSAGE_QUEUE_LENGTH{1};
     static constexpr uint16_t pin{6};
+
+    void extendSolenoid(const std_msgs::Bool::ConstPtr& msg);
 };
 
 #endif
