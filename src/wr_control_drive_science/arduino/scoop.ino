a629d2e57c25e208bd07a91e9e27e5b9fcf90a14
srao32 <srao32@wisc.edu>
2023-05-17 19:32:46 -0500

    Added Arduino code which controls the scoop servo

---
+++ src/wr_control_drive_science/arduino/scoop.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+
+// Constants
+#define SCOOP_SERVO_PIN 9
+constexpr uint8_t CODE_WORD{0xAA};
+
+// Globals
+uint8_t servoPos = 0;
+Servo scoopServo;
+
+void setup() {
+    // Initialize serial
+    Serial.begin(115200);
+
+    // Initialize servo
+    scoopServo.attach(SCOOP_SERVO_PIN);
+    scoopServo.write(0);
+
+    // Wait for code word from Pi so that we know it has booted
+    while(true) {
+        if(Serial.available()) {
+            Serial.readBytes(&servoPos, 1);
+            
+            // Break when the code word is received
+            if(servoPos == CODE_WORD) break;
+        }
+    }
+}
+
+void loop() {
+    // When a servo position is avaiable from serial, update the servo's position
+    if(Serial.available()) {
+        Serial.readBytes(&servoPos, 1);
+        scoopServo.write(servoPos);
+    }
+    
+    // Wait just a little bit to prevent slow down looping just a bit
+    delay(2);
+}
