54e90a5706992278c465d31d214eb43ce05703ee
Ben Nowotny <bennowotny65535@gmail.com>
2023-05-26 20:58:59 -0500

    refactoring the CoordinateManager to manage its own responsibilities

--- src/wr_logic_ai/src/wr_logic_ai/coordinate_manager.py
+++ src/wr_logic_ai/src/wr_logic_ai/coordinate_manager.py
@@ -1,10 +1,10 @@
 import json
 from pathlib import Path
 
+
 class CoordinateManager:
     coordinates = []
     ptr = 0
-    maxPtr = 0
 
     def __init__(self) -> None:
         pass
@@ -12,10 +12,10 @@ class CoordinateManager:
     @staticmethod
     def get_coordinate():
         return CoordinateManager.coordinates[CoordinateManager.ptr]
-        
+
     @staticmethod
     def previous_coordinate():
-        CoordinateManager.ptr = max(0,CoordinateManager.ptr - 1)
+        CoordinateManager.ptr = max(0, CoordinateManager.ptr - 1)
 
     @staticmethod
     def next_coordinate() -> bool:
@@ -23,14 +23,8 @@ class CoordinateManager:
         return CoordinateManager.ptr >= len(CoordinateManager.coordinates)
 
     @staticmethod
-    def short_range_complete() -> bool:
-        return CoordinateManager.ptr < CoordinateManager.maxPtr
-
-    @staticmethod
     def read_coordinates_file():
         dirname = Path(__file__).parents[0]
         file_name = Path.joinpath(dirname, 'coordinates.json')
         file = open(file_name, 'r').read()
         CoordinateManager.coordinates = json.loads(file)
-   
-    
