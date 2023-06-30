0be7f20a7a062aeb4a169232cdd6457ff8458507
Ben Nowotny <bennowotny65535@gmail.com>
2023-06-29 22:00:07 -0500

    Refining `wr_led_matrix` documentation

--- src/wr_led_matrix/src/led_matrix.py
+++ src/wr_led_matrix/src/led_matrix.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 # Official ROS Python code tutorial for this thing (has example code, the service server part): https://wiki.ros.org/ROS/Tutorials/WritingServiceClient%28python%29
+
+## @brief Provides a service to communicate with an Arduino to set LED panel colors
+
+##@defgroup wr_led_matrix wr_led_matrix
+# @{
+
+
 import rospy
 import serial
 from wr_led_matrix.srv import led_matrix, led_matrixRequest, led_matrixResponse
 
-s: serial.Serial
+## Serial line to the Arduino
+s: serial.Serial = None
 
 
 def handle_leds(req: led_matrixRequest):
@@ -35,10 +43,13 @@ if __name__ == "__main__":
         magic_word = s.read(8)
         if magic_word != bytes([0x31, 0x41, 0x59, 0x26, 0xDE, 0xAD, 0xBE, 0xEF]):
             rospy.logfatal(
-                f"Received incorrect magic word ({magic_word}) on startup, exiting...")
+                f"Received incorrect magic word ({magic_word}) on startup, exiting..."
+            )
             exit(1)
-        rospy.Service('led_matrix', led_matrix, handle_leds)
+        rospy.Service("led_matrix", led_matrix, handle_leds)
         # ROS Spin indefinitely (the heavy lifting is in the service callback)
         rospy.loginfo("Serial port open, starting spin")
         rospy.spin()
         s.write(bytearray([0, 0, 0, 0]))  # Shut off LED panel
+
+## @}
