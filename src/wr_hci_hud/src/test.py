510cf8fbfe3c9b025fb07e2b4666346a36d4e493
Giminer006 <96948230+Giminer006@users.noreply.github.com>
2023-12-07 19:32:18 -0600

    Diagnostic and Placeholder

--- src/wr_hci_hud/src/test.py
+++ src/wr_hci_hud/src/test.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
