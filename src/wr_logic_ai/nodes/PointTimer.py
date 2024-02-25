7a6ebd2f6a40992dd21cb808c226fc37dce014fb
Rus Milovanovic <lazarusmilov@gmail.com>
2024-02-25 14:08:18 -0600

    Removed print statements and made variable names more descriptive.

--- src/wr_logic_ai/nodes/PointTimer.py
+++ src/wr_logic_ai/nodes/PointTimer.py
@@ -1,8 +1,6 @@
 class PointTimer:
 
   def calculate_time(distance):
-    #Sooooo, As far as I remember, we were gonna get the distance from list of dictionaries. are we going to reference that
-    #when calling this function in the parameter distance or do i do that inside the function.
     speed = 0.5
     time = distance/speed
     return time
--- src/wr_logic_ai/nodes/StateTimer.py
+++ src/wr_logic_ai/nodes/StateTimer.py
@@ -5,31 +5,32 @@ class StateTimer:
     @staticmethod
     def round_up_to_multiple_of_4(number):
         rounded_up = math.ceil(number)
+
         if rounded_up % 4 != 0:
             rounded_up += 4 - (rounded_up % 4)
+
         return rounded_up
 
     @staticmethod
     def calculate_time(radius = 20):
         speed = 0.5
-        distance = 0
-        final = 0
+        total_distance = 0
+        last_path = 0
+
         for i in range(8, ((StateTimer.round_up_to_multiple_of_4(radius)) * 4) + 1, 8):
-            distance = distance + i
-            final = i
-        print(distance)
-        print(final)
-        final1 = final + 8
-        print(final1)
-        distance1 = distance + final1/2
-        print(distance1)
-        totalTime = distance1/speed
-        print(f"State Timer is {totalTime}")
+            total_distance += i
+            last_path = i
+            
+        last_path += 8
+        total_distance += last_path/2
+        total_time = total_distance/speed
+
+        return total_time
 
 
-    @staticmethod
-    def test_timer():
-        StateTimer.calculateTime(20)
+    # @staticmethod
+    # def test_timer():
+    #     StateTimer.calculateTime(20)
 
 # # Test the timer
 # StateTimer.testTimer()
