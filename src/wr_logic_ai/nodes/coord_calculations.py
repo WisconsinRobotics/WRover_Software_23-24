2ba991a099246cedc4b91b1949008fd69fbb3f6a
Rus Milovanovic <lazarusmilov@gmail.com>
2024-02-29 19:08:21 -0600

    Added function headers, moved all calculations to one function, and removed unnecessary variables.

--- src/wr_logic_ai/nodes/coord_calculations.py
+++ src/wr_logic_ai/nodes/coord_calculations.py
@@ -12,16 +12,12 @@ class CoordCalculations:
     Methods(s):
         get_coords(start_lat, start_long, distance, num_vertices) - gets the list of coordinates
         calc_dest_coord(curr_lat, curr_long, distance, bearing) - calculates the target coordinate
-        calc_north_coord(curr_lat_rad, curr_long_rad, distance) - calculates coordinate going north
-        calc_east_coord(curr_lat_rad, curr_long_rad, distance) - calculates coordinate going east
-        calc_south_coord(curr_lat_rad, curr_long_rad, distance) - calculates coordinate going south
-        calc_west_coord(curr_lat_rad, curr_long_rad, distance) - calculates coordinate going west
     '''
     
-    EARTH_RADIUS = 6378100 # in meters
+    EARTH_RADIUS = 6378100 # meters
 
     @staticmethod
-    def get_coords(start_lat, start_long, distance, num_vertices):
+    def get_coords(start_lat, start_long, distance, num_vertices) -> list:
         '''
         Description: 'Main' method that gets the coordinates the rover will travel to in the current 
             searching round based on the parameters. We only need to call this method.
@@ -36,7 +32,7 @@ class CoordCalculations:
         Return(s):
             coords - list of coordinates the rover will travel to during the search state
         '''
-        orig_dist = distance
+        cumm_dist = distance
         coords = []
         bearing = 0
         mult = 0
@@ -47,18 +43,41 @@ class CoordCalculations:
         for i in range(num_vertices):
             # update the distance after it has been traveled twice to create the square spiral shape
             if (i > 1) and (i % 2) == 0:
-                mult += orig_dist
+                cumm_dist += distance
 
             coords.append(CoordCalculations.calc_dest_coord(coords[i]['lat'], coords[i]['long'], 
-                    distance + mult, bearing))
+                    cumm_dist, bearing))
             bearing += 90
 
         return coords
 
     @staticmethod
-    def calc_dest_coord(curr_lat, curr_long, distance, bearing):
+    def calc_dest_coord(curr_lat, curr_long, distance, bearing) -> dict:
         '''
-        Description: Calls the respective cardinal direction method based on the bearing.
+        Description: Calculates the target latitude and longitude for each cardinal direction 
+            based on the current latitude and longitude, distance, and bearing.
+
+        The formula 'Destination point given distance and bearing from start point' from 
+        https://www.movable-type.co.uk/scripts/latlong.html. Based on these formulas, north 
+        has a bearing of 0°, east has a bearing of 90°, south has a bearing of 180°, 
+        and west has a bearing of 270°. 
+        
+        With the variables curr_lat, curr_long, distance, bearing, and EARTH_RADIUS, 
+            target_lat and target_long can be calculated using these formulas:
+        
+            target_lat = asin(
+                sin(curr_lat) 
+                ⋅ cos(distance / EARTH_RADIUS) 
+                + cos(curr_lat) 
+                ⋅ sin(distance / EARTH_RADIUS) 
+                ⋅ cos(bearing))
+        
+            target_long = curr_long + atan2(
+                sin(bearing) 
+                ⋅ sin(distance / EARTH_RADIUS) 
+                ⋅ cos(curr_lat), 
+                cos(distance / EARTH_RADIUS) 
+                − sin(curr_lat)^2))
 
         Parameter(s):
             curr_lat - the current latitude in degrees
@@ -77,23 +96,46 @@ class CoordCalculations:
         target_lat_rad = 0
         target_long_rad = 0
 
-         # convert current latitude and longitude from degrees to radians
+        # convert current latitude and longitude from degrees to radians
         curr_lat_rad = math.radians(curr_lat)
         curr_long_rad = math.radians(curr_long)
 
-        # call respective direction method
+        # north
         if bearing % 360 == 0:
-            target_lat_rad, target_long_rad = CoordCalculations.calc_north_coord(
-                    curr_lat_rad, curr_long_rad, distance)
+            target_lat_rad = target_lat_rad = math.asin(
+                math.sin(curr_lat_rad) 
+                * math.cos(distance / CoordCalculations.EARTH_RADIUS) 
+                + math.cos(curr_lat_rad) 
+                * math.sin(distance / CoordCalculations.EARTH_RADIUS) 
+                * 1)
+            target_long_rad = curr_long_rad # does not change
+        # east
         elif bearing % 360 == 90:
-            target_lat_rad, target_long_rad = CoordCalculations.calc_east_coord(
-                    curr_lat_rad, curr_long_rad, distance)
+            target_lat_rad = curr_lat_rad # does not change
+            target_long_rad = curr_long_rad + math.atan2(
+                1 
+                * math.sin(distance / CoordCalculations.EARTH_RADIUS) 
+                * math.cos(curr_lat_rad), 
+                math.cos(distance / CoordCalculations.EARTH_RADIUS) 
+                - math.sin(curr_lat_rad) ** 2)
+        # south
         elif bearing % 360 == 180:
-            target_lat_rad, target_long_rad = CoordCalculations.calc_south_coord(
-                    curr_lat_rad, curr_long_rad, distance)
+            target_lat_rad = math.asin(
+                math.sin(curr_lat_rad) 
+                * math.cos(distance / CoordCalculations.EARTH_RADIUS) 
+                + math.cos(curr_lat_rad) 
+                * math.sin(distance / CoordCalculations.EARTH_RADIUS) 
+                * -1)
+            target_long_rad = curr_long_rad # does not change
+        # west
         elif bearing % 360 == 270:
-            target_lat_rad, target_long_rad = CoordCalculations.calc_west_coord(
-                    curr_lat_rad, curr_long_rad, distance)
+            target_lat_rad = curr_lat_rad # does not change
+            target_long_rad = curr_long_rad + math.atan2(
+                -1 
+                * math.sin(distance / CoordCalculations.EARTH_RADIUS) 
+                * math.cos(curr_lat_rad), 
+                math.cos(distance / CoordCalculations.EARTH_RADIUS) 
+                - math.sin(curr_lat_rad) ** 2)
         else:
             return {}
 
@@ -105,125 +147,3 @@ class CoordCalculations:
         coord = {'lat': target_lat, 'long': target_long, 'distance': distance, 'bearing': bearing % 360}
 
         return coord
-    
-    # The next four methods are based on the formula 'Destination point given distance and bearing 
-    # from start point' from https://www.movable-type.co.uk/scripts/latlong.html. Based on these 
-    # formulas, north has a bearing of 0°, east has a bearing of 90°, south has a bearing of 180°, 
-    # and west has a bearing of 270°. 
-    #
-    # With the variables curr_lat, curr_long, distance, bearing, and EARTH_RADIUS, 
-    #   target_lat and target_long can be calculated using these formulas:
-    #
-    #       target_lat = asin(
-    #           sin(curr_lat) 
-    #           ⋅ cos(distance / EARTH_RADIUS) 
-    #           + cos(curr_lat) 
-    #           ⋅ sin(distance / EARTH_RADIUS) 
-    #           ⋅ cos(bearing))
-    #
-    #       target_long = curr_long + atan2(
-    #           sin(bearing) 
-    #           ⋅ sin(distance / EARTH_RADIUS) 
-    #           ⋅ cos(curr_lat), 
-    #           cos(distance / EARTH_RADIUS) 
-    #           − sin(curr_lat)^2))
-
-    @staticmethod
-    def calc_north_coord(curr_lat_rad, curr_long_rad, distance):
-        '''
-        Description: Calculates the target latitude based on the current latitude and distance 
-            traveled north. Only latitude needs to be calculated since the longitude does not change 
-            when going north.
-
-        Parameter(s):
-            curr_lat_rad - the current latitude in radians
-            curr_long_rad - the current longitude in radians
-            distance - the distance that will be traveled
-
-        Return(s):
-            target_lat_rad - the newly calculated target latitude in radians
-            curr_long_rad - the same current longitude in radians
-        '''
-        target_lat_rad = math.asin(
-                math.sin(curr_lat_rad) 
-                * math.cos(distance / CoordCalculations.EARTH_RADIUS) 
-                + math.cos(curr_lat_rad) 
-                * math.sin(distance / CoordCalculations.EARTH_RADIUS) 
-                * 1)
-
-        return target_lat_rad, curr_long_rad
-
-    @staticmethod
-    def calc_east_coord(curr_lat_rad, curr_long_rad, distance):
-        '''
-        Description: Calculates the target longitude based on the current latitude, current longitude, 
-            and distance traveled east. Only longitude needs to be calculated since the latitude 
-            does not change when going east.
-
-        Parameter(s):
-            curr_lat_rad - the current latitude in radians
-            curr_long_rad - the current longitude in radians
-            distance - the distance that will be traveled
-
-        Return(s):
-            curr_lat_rad - the same current latitude in radians
-            target_long_rad - the newly calculated target longitude in radians
-        '''
-        target_long_rad = curr_long_rad + math.atan2(
-                1 
-                * math.sin(distance / CoordCalculations.EARTH_RADIUS) 
-                * math.cos(curr_lat_rad), 
-                math.cos(distance / CoordCalculations.EARTH_RADIUS) 
-                - math.sin(curr_lat_rad) ** 2)
-
-        return curr_lat_rad, target_long_rad
-
-    @staticmethod
-    def calc_south_coord(curr_lat_rad, curr_long_rad, distance):
-        '''
-        Description: Calculates the target latitude based on the current latitude and distance 
-            traveled south. Only latitude needs to be calculated since the longitude does not change 
-            when going south.
-
-        Parameter(s):
-            curr_lat_rad - the current latitude in radians
-            curr_long_rad - the current longitude in radians
-            distance - the distance that will be traveled
-
-        Return(s):
-            target_lat_rad - the newly calculated target latitude in radians
-            curr_long_rad - the same current longitude in radians
-        '''
-        target_lat_rad = math.asin(
-                math.sin(curr_lat_rad) 
-                * math.cos(distance / CoordCalculations.EARTH_RADIUS) 
-                + math.cos(curr_lat_rad) 
-                * math.sin(distance / CoordCalculations.EARTH_RADIUS) 
-                * -1)
-
-        return target_lat_rad, curr_long_rad
-
-    @staticmethod
-    def calc_west_coord(curr_lat_rad, curr_long_rad, distance):
-        '''
-        Description: Calculates the target longitude based on the current latitude, current longitude, 
-            and distance traveled west. Only longitude needs to be calculated since the latitude 
-            does not change when going west.
-
-        Parameter(s):
-            curr_lat_rad - the current latitude in radians
-            curr_long_rad - the current longitude in radians
-            distance - the distance that will be traveled
-
-        Return(s):
-            curr_lat_rad - the same current latitude in radians
-            target_long_rad - the newly calculated target longitude in radians
-        '''
-        target_long_rad = curr_long_rad + math.atan2(
-                -1 
-                * math.sin(distance / CoordCalculations.EARTH_RADIUS) 
-                * math.cos(curr_lat_rad), 
-                math.cos(distance / CoordCalculations.EARTH_RADIUS) 
-                - math.sin(curr_lat_rad) ** 2)
-
-        return curr_lat_rad, target_long_rad
