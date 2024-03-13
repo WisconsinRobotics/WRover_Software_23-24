e8ff00b36f144b13af4bb8c9e64d543b2c6469c3
Kyle Sha <kylesha585@gmail.com>
2024-03-13 23:39:15 +0000

    remove extra debug logging

--- src/wr_hsi_sensing/nodes/gps_forwarding_node.py
+++ src/wr_hsi_sensing/nodes/gps_forwarding_node.py
@@ -16,10 +16,6 @@ def gps_callback(msg):
     gps_coord_data = CoordinateMsg()
     gps_coord_data.latitude = msg.latitude
     gps_coord_data.longitude = msg.longitude
-
-    print("latitude : {}".format(msg.latitude))
-    print("longitude: {}".format(msg.longitude))
-
     coord_data_publisher.publish(gps_coord_data)
 
 def gps_data_subscriber():
