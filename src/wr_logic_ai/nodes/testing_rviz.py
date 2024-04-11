f7aec4bcd870a76e3a3cfec0cf60769cc4faf575
Jay Sangani <jsangani@wisc.edu>
2024-04-10 20:07:24 -0500

    refactoring testing rviz to fix it

--- src/wr_logic_ai/nodes/testing_rviz.py
+++ src/wr_logic_ai/nodes/testing_rviz.py
@@ -16,6 +16,32 @@ from visualization_msgs.msg import Marker
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Float64
 
+global drive_pub
+global smoothing_constant
+global speed_factor
+global heading_pub
+global frameCount
+global heading_msg
+global actual_heading_msg
+global actual_heading_pub
+global delta_heading_pub
+global delta_heading_msg
+global marker
+global marker_pub
+global marker_circle
+global marker_circle_pub
+global marker_flag
+global marker_flag_pub
+global laser_adjuster_pub
+global wRover
+global wRover_pub
+global current_lat
+global current_long
+global target_angle
+global target_sector
+global NAV_THRESH_DISTANCE
+# global delta_heading
+
 # Navigation parameters
 # distance before obstacle avoidance logics is triggered (in meters)
 LIDAR_THRESH_DISTANCE = 5
@@ -30,12 +56,120 @@ current_long = 0
 # target location
 target_angle = 0
 target_sector = 0
+frameCount = 0
 
 # used for obtaining navigation angle and valleys
 smoothing_constant = 3
 
 # global speed factor updated through navigation
 speed_factor = 0
+
+# Publish data out to the marker 
+wRover_pub = rospy.Publisher('wRover_marker', Marker, queue_size=10)
+
+# TESTING
+heading_pub = rospy.Publisher("/debug_heading", PoseStamped, queue_size=1)
+heading_msg = PoseStamped()
+heading_msg.pose.position.x = 0
+heading_msg.pose.position.y = 0
+heading_msg.pose.position.z = 0
+heading_msg.pose.orientation.x = 0
+heading_msg.pose.orientation.y = 0
+heading_msg.header.frame_id = "laser"
+frameCount = 0
+
+    # TESING
+actual_heading_pub = rospy.Publisher('/actual_heading', PoseStamped, queue_size=1)
+actual_heading_msg = PoseStamped()
+actual_heading_msg.pose.position.x = 0
+actual_heading_msg.pose.position.y = 0
+actual_heading_msg.pose.position.z = 0
+actual_heading_msg.pose.orientation.x = 0
+actual_heading_msg.pose.orientation.y = 0
+actual_heading_msg.header.frame_id = "laser"
+
+    # TESING
+delta_heading_pub = rospy.Publisher('/delta_heading', PoseStamped, queue_size=1)
+delta_heading_msg = PoseStamped()
+delta_heading_msg.pose.position.x = 0
+delta_heading_msg.pose.position.y = 0
+delta_heading_msg.pose.position.z = 0
+delta_heading_msg.pose.orientation.x = 0
+delta_heading_msg.pose.orientation.y = 0
+delta_heading_msg.header.frame_id = "laser"
+
+
+laser_adjuster_pub = rospy.Publisher('/laser_adjuster', Float64, queue_size=1)
+
+# instantiate all directional markers
+marker_pub = rospy.Publisher("pose_marker", Marker, queue_size=1)
+marker = Marker()
+marker.header.frame_id = "laser"  # Set your frame_id
+marker.type = Marker.CUBE
+marker.action = Marker.ADD
+marker.scale.x = 10.0  # Set the dimensions of the plane
+marker.scale.y = 10.0
+marker.scale.z = 0.01  # Thickness of the plane
+marker.color.a = 0.5
+marker.color.r = .1
+marker.color.g = .6
+marker.color.b = .1
+marker.pose = delta_heading_msg.pose  # Set the position and orientation based on your pose
+
+marker_circle_pub = rospy.Publisher("pose_circle_marker", Marker, queue_size=1)
+marker_circle = Marker()
+marker_circle.header.frame_id = "laser"  # Set your frame_id
+marker_circle.type = Marker.SPHERE
+marker_circle.action = Marker.ADD
+marker_circle.scale.x = LIDAR_THRESH_DISTANCE*2  # Set the dimensions of the plane
+marker_circle.scale.y = LIDAR_THRESH_DISTANCE*2
+marker_circle.scale.z = 0.01  # Thickness of the plane
+marker_circle.color.a = 0.5
+marker_circle.color.r = .1
+marker_circle.color.g = .3
+marker_circle.color.b = .6
+marker_circle.pose = delta_heading_msg.pose  # Set the position and orientation based on your pose
+
+wRover = Marker()
+wRover.header.frame_id = "laser"  # Replace with your fixed frame
+wRover.type = Marker.MESH_RESOURCE
+wRover.mesh_resource = "package://wr_logic_ai/meshes/Eclipse_Base_Simple.stl"  # Replace with your 3D object path
+wRover.pose.position.x = 0.0  # Replace with your desired position
+wRover.pose.position.y = 0.0
+wRover.pose.position.z = .11
+wRover.pose.orientation.x = 0.0  # Replace with your desired orientation
+wRover.pose.orientation.y = 0.0
+wRover.pose.orientation.z = 0.0
+wRover.pose.orientation.w = 1.0
+wRover.scale.x = 0.01  # Replace with your desired scale
+wRover.scale.y = 0.01
+wRover.scale.z = 0.01
+wRover.color.a = 1.0
+wRover.color.r = 1.0
+wRover.color.g = 0.0
+wRover.color.b = 0.0
+# wRover_pub.publish(wRover)
+
+# creates marker for target
+marker_flag_pub = rospy.Publisher('flag_marker', Marker, queue_size=1)
+marker_flag = Marker()
+marker_flag.header.frame_id = "laser"  # Replace with your fixed frame
+marker_flag.type = Marker.MESH_RESOURCE
+marker_flag.mesh_resource = "package://wr_logic_ai/meshes/flag.stl"  # Replace with your 3D object path
+marker_flag.pose.position.x = 0.0  # Replace with your desired position
+marker_flag.pose.position.y = 5.0
+marker_flag.pose.position.z = .11
+marker_flag.pose.orientation.x = 0.0  # Replace with your desired orientation
+marker_flag.pose.orientation.y = 0.0
+marker_flag.pose.orientation.z = 0.0
+marker_flag.pose.orientation.w = 1.0
+marker_flag.scale.x = 0.02  # Replace with your desired scale
+marker_flag.scale.y = 0.02
+marker_flag.scale.z = 0.02
+marker_flag.color.a = 1.0
+marker_flag.color.r = 1.0
+marker_flag.color.g = 0.0
+marker_flag.color.b = 0.0
 
 # Start the tasks managed to drive autonomously
 def initialize() -> None:
@@ -73,7 +207,35 @@ def initialize() -> None:
     global laser_adjuster_pub
     global wRover
     global wRover_pub
+    global current_lat
+    global current_long
+    global target_angle
+    global target_sector
+    global NAV_THRESH_DISTANCE
     global delta_heading
+
+
+    # Navigation parameters
+    # distance before obstacle avoidance logics is triggered (in meters)
+    LIDAR_THRESH_DISTANCE = 5
+    # distance before rover believes it has reached the target (in meters)
+    NAV_THRESH_DISTANCE = 0.5
+
+    # initialize target angle to move forward
+    # current location
+    current_lat = 0
+    current_long = 0
+
+    # target location
+    target_angle = 0
+    target_sector = 0
+    frameCount = 0
+
+    # used for obtaining navigation angle and valleys
+    smoothing_constant = 3
+
+    # global speed factor updated through navigation
+    speed_factor = 0
 
     # Publish data out to the marker 
     wRover_pub = rospy.Publisher('wRover_marker', Marker, queue_size=10)
@@ -182,6 +344,7 @@ def initialize() -> None:
     marker_flag.color.g = 0.0
     marker_flag.color.b = 0.0
     
+    
 def update_navigation_rviz_sim(delta_heading: Float64, result: Float64, cur_heading: Float64) -> None:
     '''
     Configures heading and poses for rviz simulation. Values either passed from fake data or real data 
@@ -192,6 +355,7 @@ def update_navigation_rviz_sim(delta_heading: Float64, result: Float64, cur_head
         of robot being 0 (Counterclockwise).
     @param cur_heading(Float64): Value of the current direction of the rover relative to East at 0
     '''
+
     if rospy.get_param("/long_range_action_server/wrover_hw") == "MOCK":
         heading_msg.header.seq = frameCount
         heading_msg.header.stamp = rospy.get_rostime()
@@ -279,7 +443,6 @@ def update_navigation_rviz_sim(delta_heading: Float64, result: Float64, cur_head
 if __name__ == "__main__":
     try:
         # Initialize the running environment for this program
-        initialize()
         # Spin RosPy to the next update cycle
         rospy.spin()
         
