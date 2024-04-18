53d1289c054cb3832e60cadc8c35c1b1156d1563
Colin Li <clikingofinfo@gmail.com>
2024-04-17 19:46:03 -0500

    Run freefall using gamepad

--- src/wr_logic_teleop_ds/src/TeleopDriveFreefall.py
+++ src/wr_logic_teleop_ds/src/TeleopDriveFreefall.py
@@ -1,30 +1,37 @@
+#!/usr/bin/env python
+
 import rospy
+from std_msgs.msg import Float32
 from wr_drive_msgs.msg import DriveTrainCmd
 from wreadinput import DeviceAxis
 
+rospy.init_node('TeleopDriveFreefall', anonymous=True)
 
-left_drive_pub = rospy.Publisher("/control/drive_system/cmd", DriveTrainCmd, queue_size=10)
-right_drive_pub = rospy.Publisher("/control/drive_system/cmd", DriveTrainCmd, queue_size=10)
+drive_pub = rospy.Publisher("/control/drive_system/cmd", DriveTrainCmd, queue_size=10)
 
+left_value = 0
+right_value = 0
 
+def left_callback(msg: Float32):
+    global left_value
+    left_value = -msg.data
+
+def right_callback(msg: Float32):
+    global right_value
+    right_value = -msg.data
 
 def pub_drive_freefall():
-   
+
     rate = rospy.Rate(10) # 10hz
-    rospy.Subscriber("/hci/freefall/gamepad/stick_right_y", left_callback)
-    rospy.Subscriber("/hci/freefall/gamepad/stick_left_y", right_callback)
+    rospy.Subscriber("/hci/freefall/gamepad/axis/stick_left_y", Float32, left_callback)
+    rospy.Subscriber("/hci/freefall/gamepad/axis/stick_right_y", Float32, right_callback)
 
-def left_callback(msg):
-    left_drive_pub.publish = -1*msg.data
-
-def right_callback(msg):
-    right_drive_pub.publish = -1*msg.data
-
-
+    while not rospy.is_shutdown():
+        drive_pub.publish(left_value, right_value)
+        rate.sleep()
 
 if __name__ == '__main__':
     try:
-         rospy.init_node('pub_drive_freefall', anonymous=True)
          pub_drive_freefall()
     except rospy.ROSInterruptException:
         pass
