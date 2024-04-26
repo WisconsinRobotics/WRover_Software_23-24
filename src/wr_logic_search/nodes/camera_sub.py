2dcfc30a04b44fe55badfd646b3f772af448c936
Rus Milovanovic <lazarusmilov@gmail.com>
2024-04-25 19:52:44 -0500

    Made comments. Put the correct message type. Removed class. Calling initialize() in action server.

--- src/wr_logic_search/nodes/camera_sub.py
+++ src/wr_logic_search/nodes/camera_sub.py
@@ -5,49 +5,51 @@
 @{
 @defgroup wr_logic_search_camera_sub Camera Subscriber
 @brief Subscriber for the camera used in object detection
-@details Subscribes the to the placeholder camera topic. It is assumed there is a separate file that does object detection. 
-If the object is detected, it will return True to the Search Pattern action server. False otherwise. This Subscriber node
-means search_pattern_client.py and search_pattern_server.py can be removed.
+@details Subscribes the to the placeholder camera topic. It is assumed there is a 
+separate file that does object detection. If the object is detected, it will 
+return True to the Search Pattern action server. False otherwise. This Subscriber 
+node means search_pattern_client.py and search_pattern_server.py can be removed.
 @{
 """
 
 import rospy
 from std_msgs.msg import Bool
 
-class CameraSub:
+object_detected = False
 
-    object_detected = False
+'''
+Description: Initializes the Subscriber node that listens to the 
+    camera/object_detection topic.
+'''
+def initialize():
+    rospy.init_node('object_detection_subscriber', anonymous=True)
+    rospy.Subscriber('camera/object_detection', Bool, object_detection_callback)
 
-    '''
-    Description: Function that receives the data from the camera topic and saves the result of object detection.
+'''
+Description: Receives the data from the camera/object_detection topic and saves 
+    the result of object detection.
 
-    Parameter(s):
-        data - the bool variable containing the status of object detection, True if target object detected, False otherwise
-    '''
-    def object_detection_callback(data):
-        global object_detected
-        object_detected = data.data
+Parameter(s):
+    data - the bool variable containing the status of object detection, 
+        True if target object detected, False otherwise
+'''
+def object_detection_callback(data):
+    global object_detected
+    object_detected = data.data
 
-    '''
-    Description: Function that listens to the camera topic to check if it has detected the target object.
-    '''
-    def listener():
-        rospy.init_node('object_detection_subscriber', anonymous=True)
-        rospy.Subscriber('camera/object_detection', Bool, CameraSub.object_detection_callback)
-        rospy.spin()
+'''
+Description: Returns the status of object_detected.
 
-    '''
-    Description: Function that returns the status of object_detected.
-
-    Return(s):
-        object_detected - True if target object was detected, False otherwise
-    '''
-    def get_detection_result():
-        global object_detected
-        return object_detected
+Return(s):
+    object_detected - True if target object was detected, False otherwise
+'''
+def get_detection_result():
+    global object_detected
+    return object_detected
 
 if __name__ == '__main__':
     try:
-        CameraSub.listener()
+        initialize()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
--- src/wr_logic_search/nodes/search_action_server.py
+++ src/wr_logic_search/nodes/search_action_server.py
@@ -4,22 +4,22 @@
 @defgroup wr_logic_search
 @{
 @defgroup wr_logic_search_search_action_server Search Action Server
-@brief Tells the rover travel to the target coordinates and calls obstacle avoidance to make sure the rover can actually reach these coordinates.
-@details Enters a while loop that continually calls obstacle avoidance to update the target to the next coordinate in the list. 
-For each coordinate, if the time between the starting time and the current time go above the estimated travel time for that
-specific coordinate, the coordinate is assumed to be unreachable, and the rover will skip it and move on to the next coordinate.
+@brief Action server for the rover search pattern
+@details Tells the rover to travel to specific coordinates that follow the square 
+spiral search pattern. Keeps track of time to know when a coordinate should be 
+skipped due to large, unavoidable obstacles. At each coordinate, the camera scans 
+the surrounding area, searching for the target object of the current round.
 @{
 """
 
 import rospy
 import actionlib
-from wr_logic_search.msg import SearchStateAction, SearchStateGoal
+from wr_logic_search.msg import SearchStateAction
 from wr_logic_longrange.nodes import obstacle_avoidance
 import coord_calculations
 import travel_timer
-# from wr_logic_search.srv import SearchPatternService
-# from camera_sub import CameraSub
-from coord_sub import CoordSub
+# import coord_sub
+# import camera_sub 
 
 class SearchActionServer(object):
     def __init__(self, name) -> None:
@@ -31,6 +31,8 @@ class SearchActionServer(object):
 
         self._action_name = name
         obstacle_avoidance.initialize()
+        # coord_sub.initialize()
+        # camera_sub.initialize()
         self._as = actionlib.SimpleActionServer(
             self._action_name,
             SearchStateAction,
@@ -39,28 +41,28 @@ class SearchActionServer(object):
         )
         self._as.start()
 
-    def execute_callback(self): #, goal: SearchStateGoal):
+    def execute_callback(self): 
         """
-        Executes the long range obstacle avoidance code, and triggers the corresponding state machine event
-        depending on the result of the navigation
+        Executes the long range obstacle avoidance code, and triggers the 
+        corresponding state machine event depending on the result of the 
+        navigation.
 
-        @param goal (SearchGoal): Goal for the navigation segment, which contains the GPS coordinates
-        of the target
+        @param goal (SearchGoal): Goal for the navigation segment, which contains 
+        the GPS coordinates of the target.
         """
         distance = 4
         num_vertices = 22
 
-        # coord_sub = CoordSub()
-        # coord = coord_sub.get_coord_result()
-        # start_lat =  coord.latitude
-        # start_long = coord.longitude
+        # start_lat, start_long = coord_sub.get_coord_result()
 
-        # TODO: During testing, hard code some coordinates.
+        # TODO: During testing, hard-code some coordinates.
         hard_coded_lat = 0
         hard_coded_long = 0
 
-        # coords = coord_calculations.get_coords(goal.starting_lat, goal.starting_long, distance, num_vertices)
-        coords = coord_calculations.get_coords(hard_coded_lat, hard_coded_long, distance, num_vertices)
+        # coords = coord_calculations.get_coords(
+        #     start_lat, start_long, distance, num_vertices)
+        coords = coord_calculations.get_coords(
+            hard_coded_lat, hard_coded_long, distance, num_vertices)
         SEARCH_TIMEOUT_TIME = travel_timer.calc_state_time() # default = 20 meters
 
         i = 0
@@ -68,27 +70,21 @@ class SearchActionServer(object):
         point_time = rospy.get_rostime()
         while (
             rospy.get_rostime() - state_time < SEARCH_TIMEOUT_TIME
-            and rospy.get_rostime() - point_time < travel_timer.calc_point_time(coords[i]['distance'])
+            and rospy.get_rostime() - point_time < travel_timer.calc_point_time(
+                coords[i]['distance'])
             and not rospy.is_shutdown()
             and i < num_vertices
         ):
-            if obstacle_avoidance.update_target(coords[i]['lat'], coords[i]['long']) == False:
+            if obstacle_avoidance.update_target(
+                coords[i]['lat'], coords[i]['long']) == False:
                 # return self._as.set_succeeded()
                 return self._as.set_aborted()
-                    
-            # camera_sub = CameraSub()
-            # if camera_sub.get_detection_result:
+            
+            # if camera_sub.get_detection_result():
             #     break
 
             i += 1
             point_time = rospy.get_rostime()
-            
-            # # Camera Service - still not sure about integrating the camera with the state machine
-            # camera_service = rospy.ServiceProxy('search_pattern_service', SearchPatternService)
-            # camera_service.wait_for_service('search_pattern_service')
-
-            # if camera_service:
-            #     break # what should be done when the target object is found? how should we enter ShortRange?
 
 if __name__ == "__main__":
     try:
