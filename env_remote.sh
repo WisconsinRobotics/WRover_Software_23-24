5640962acd990a8362f380eb4d0ae92979921e39
Colin Li <clikingofinfo@gmail.com>
2024-05-10 18:27:39 -0500

    updated launch file wiht gps

--- env_remote.sh
+++ env_remote.sh
@@ -9,7 +9,7 @@ CATKIN_SHELL=sh
 _CATKIN_SETUP_DIR=$(cd "`dirname "$0"`" > /dev/null && pwd)
 . "$_CATKIN_SETUP_DIR/setup.sh"
 
-export ROS_MASTER/_URI='http://wrover.local:11311'
+export ROS_MASTER_URI='http://wrover.local:11311'
 export ROS_IP=`ip addr show eth0 | grep -Po 'inet [\d.]+' | awk '{print $2}'`
 export ROS_HOSTNAME='wrover.local'
 
