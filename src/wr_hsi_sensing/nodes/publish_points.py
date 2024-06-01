458c634876e3fb53a2b9291724dddcf99d63ec10
Colin Li <clikingofinfo@gmail.com>
2024-06-01 00:08:53 -0500

    Added visualizing points in mapviz

--- src/wr_hsi_sensing/nodes/publish_points.py
+++ src/wr_hsi_sensing/nodes/publish_points.py
@@ -10,23 +10,31 @@
 import rospy
 from sensor_msgs.msg import NavSatFix
 from visualization_msgs.msg import Marker
-from visualization_msgs.msg import MarkerArray
+#from visualization_msgs.msg import MarkerArray
 from geometry_msgs.msg import Point
 
+import json
+from pathlib import Path
 
 
 def main():
-    rospy.init_node("gps_data_subscriber_node", anonymous = False)
+    #rospy.init_node("gps_data_subscriber_node", anonymous = False)
 
     coord_data_publisher = rospy.Publisher("/navsat/fix", NavSatFix, queue_size=1)
     
-    lat1 = 38.371768
-    long1 = -110.704255
-    coord_data_publisher.publish(NavSatFix(latitde=lat1,longitude=long1))
-    
-    lat2 = 38.371240
-    long2 = -110.704195
-    coord_data_publisher.publish(NavSatFix(latitde=lat2,longitude=long2))
+    dirname = Path(__file__).parents[0]
+    file_name = Path.joinpath(dirname, "coordinates.json")
+    file = open(file_name, "r").read()
+    coordinates = json.loads(file)
+
+    rospy.loginfo(coordinates)
+
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        for coord in coordinates:
+            rospy.loginfo(coord)
+            coord_data_publisher.publish(NavSatFix(latitude=coord["lat"],longitude=coord["long"]))
+        rate.sleep()
         
 def publish_markers():
     # locations = MarkerArray()
@@ -36,17 +44,21 @@ def publish_markers():
     markers = []
     lat1 = 38.371768
     long1 = -110.704255
-    markers.add(Marker(points=[Point(y = lat1, x = long1)]))
+    markers.append(Marker(points=[Point(y = lat1, x = long1)]))
     
     lat2 = 38.371240
     long2 = -110.704195
-    markers.add(Marker(points=[Point(y = lat2, x = long2)]))
+    markers.append(Marker(points=[Point(y = lat2, x = long2)]))
     
-    coord_data_publisher.publish(MarkerArray(markers))
+    rate = rospy.Rate(10)
+    while not rospy.is_shutdown():
+        coord_data_publisher.publish(MarkerArray(markers))
+        
     
     
     
 if(__name__ == "__main__"):
     rospy.init_node("gps_data_points_pub", anonymous = False)
-    publish_markers()
-    rospy.spin()
+    #publish_markers()
+    main()
+    #rospy.spin()
